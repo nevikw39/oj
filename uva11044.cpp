c8e10d9203da4e7eab796490ad5f3d2b21ecc545
nevikw39 <nevikw39@icloud.com>
2020-06-28 01:38:55 +0800

    uva11044

---
+++ uva11044.cpp
@@ -0,0 +1,22 @@
+#include <bits/extc++.h>
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#endif
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        short m, n;
+        cin >> m >> n;
+        cout << (m / 3) * (n / 3) << endl;
+    }
+    return 0;
+}
