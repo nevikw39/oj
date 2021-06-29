90dfe30140dbc7eeea389490485103648b983358
nevikw39 <nevik.w39@gmail.com>
2021-06-29 21:14:47 +0800

    P-2-3

---
+++ ap325/d013.cpp
@@ -0,0 +1,41 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/stdc++.h>
+#ifndef nevikw39
+#define nevikw39      \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct _cerr
+{
+    template <typename T>
+    _cerr &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+int main()
+{
+    nevikw39;
+    string s;
+    long long x = 0, y, p, r = 1;
+    cin >> s >> y >> p;
+    for_each(ALL(s), [&](char c) { x = (x * 10 + c - '0') % p; });
+    for (x %= p; y; y >>= 1, x = x * x % p)
+        if (y & 1)
+            r = r * x % p;
+    cout << r << '\n';
+    return 0;
+}
