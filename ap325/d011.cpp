f0776d79999ca0443c9dc7b816e57034000a886b
nevikw39 <nevik.w39@gmail.com>
2021-06-29 20:46:31 +0800

    P-2-2

---
+++ ap325/d011.cpp
@@ -0,0 +1,43 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/stdc++.h>
+#ifndef nevikw39
+#define nevikw39      \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct _cerr
+{
+    template <typename T>
+    _cerr &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+int main()
+{
+    nevikw39;
+    int n;
+    cin >> n;
+    vector<int> v(n), w;
+    for (int &i : v)
+        cin >> i;
+    w = v;
+    sort(ALL(w));
+    auto u = unique(ALL(w));
+    for_each(ALL(v), [&](int &i) { i = lower_bound(w.begin(), u, i) - w.begin(); });
+    copy(ALL(v), ostream_iterator<int>(cout, " "));
+    return 0;
+}
