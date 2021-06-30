11e1a56e50dfe97fa6ffc09a1b4a4f1ff07c3b60
nevikw39 <nevik.w39@gmail.com>
2021-06-30 16:09:41 +0800

    P-2-9

---
+++ ap325/d018.cpp
@@ -0,0 +1,90 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#ifndef nevikw39
+#define nevikw39      \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct _cerr
+{
+    template <typename T>
+    _cerr &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = __gnu_pbds::tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+template <typename T>
+using _heap = __gnu_pbds::priority_queue<T>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename T = int64_t>
+T extgcd(T a, T b, T &x, T &y)
+{
+    if (!b)
+    {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    T g = extgcd(b, a % b, y, x);
+    y -= a / b * x;
+    return g;
+}
+template <typename T = int64_t>
+T inline inv(T a, T m)
+{
+    T x, y;
+    extgcd(a, m, x, y);
+    return (x % m + m) % m;
+}
+using citr = vector<long long>::const_iterator;
+template <typename T>
+void f(citr i, const citr &end, unordered_map<T, T> &m, T M, T p = 1LL)
+{
+    if (i >= end)
+    {
+        ++m[p];
+        return;
+    }
+    f(i + 1, end, m, M, p);
+    f(i + 1, end, m, M, p * *i % M);
+}
+int main()
+{
+    nevikw39;
+    long long n, p, r;
+    cin >> n >> p;
+    vector<long long> v(n);
+    for (auto &i : v)
+        cin >> i;
+    unordered_map<long long, long long> m1, m2;
+    f(v.cbegin(), v.cbegin() + (n >> 1), m1, p);
+    f(v.cbegin() + (n >> 1), v.cend(), m2, p);
+    r = (--m1[1] + --m2[1]) % p;
+    for (const auto &i : m1)
+    {
+        auto itr = m2.find(inv(i.ST, p));
+        if (itr != m2.end())
+            r = (r + i.ND * itr->ND) % p;
+    }
+    cout << r << '\n';
+    return 0;
+}
