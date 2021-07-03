6656bc827655cbedf4c50b2cf3766dd258354c2e
nevikw39 <nevik.w39@gmail.com>
2021-07-03 16:24:10 +0800

    Q-4-10

---
+++ ap325/d054.cpp
@@ -0,0 +1,68 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#ifndef nevikw39
+#define nevikw39      \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct _cerr
+{
+    template <typename T>
+    _cerr &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    long long n, m, f = 0, s = 0;
+    cin >> n >> m;
+    vector<long long> p(n);
+    for (auto &i : p)
+        cin >> i, s += i;
+    auto foo = [&](long long f)
+    {
+        for (long long i = 0, c = f, q = m; i < n; i++)
+        {
+            if (c < p[i])
+            {
+                if (!q--)
+                    return false;
+                else
+                    c = f;
+            }
+            if ((c -= p[i]) < 0)
+                return false;
+        }
+        return true;
+    };
+    for (long long jump = s >> 1; jump > 0; jump >>= 1)
+        while (f + jump < s && !foo(f + jump))
+            f += jump;
+    cout << f + 1 << '\n';
+    return 0;
+}
