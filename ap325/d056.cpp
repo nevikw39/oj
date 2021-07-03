8bdb0656195db23364490db23e5b61040bf71bdc
nevikw39 <nevik.w39@gmail.com>
2021-07-03 22:37:41 +0800

    P-4-15

---
+++ ap325/d056.cpp
@@ -0,0 +1,66 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#ifndef nevikw39
+#define nevikw39      \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct _cerr
+{
+    template <typename T>
+    _cerr &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int64_t n, r = INT64_MAX >> 1;
+    cin >> n;
+    vector<pair<int64_t, int64_t>> v(n);
+    for (auto &i : v)
+        cin >> i.ST >> i.ND;
+    sort(ALL(v));
+    multimap<int64_t, int64_t> mm;
+#define Y ST
+#define X ND
+    for (const auto &i : v)
+    {
+        for (auto itr = mm.lower_bound(i.ND - r); itr != mm.end() && itr->Y <= i.ND + r; itr++)
+        {
+            if (i.ST - itr->X > r)
+            {
+                itr = --mm.erase(itr);
+                continue;
+            }
+            r = min(r, abs(i.ST - itr->X) + abs(i.ND - itr->Y));
+        }
+        mm.emplace(i.ND, i.ST);
+    }
+    cout << r << '\n';
+    return 0;
+}
