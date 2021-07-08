67c850ba9caf0a062c5064436ef755b8df497787
nevikw39 <nevik.w39@gmail.com>
2021-07-08 14:47:11 +0800

    Q-6-24

---
+++ ap325/d088.cpp
@@ -0,0 +1,77 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#ifndef nevikw39
+#define nevikw39      \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct _cerr
+{
+    template <typename T>
+    _cerr &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int64_t n, r = 0;
+    cin >> n;
+    vector<int64_t> h(n + 1), v(n + 1), f(n + 1), d(n + 1), b(n + 1);
+    h[0] = INT64_MAX;
+    for (int64_t i = 1; i <= n; i++)
+        cin >> h[i];
+    for (int64_t i = 1; i <= n; i++)
+        cin >> v[i];
+    stack<int64_t> s;
+    s.push(0);
+    for (int64_t i = 1; i <= n; i++)
+    {
+        while (h[s.top()] <= h[i])
+            s.pop();
+        f[i] = s.top();
+        s.push(i);
+    }
+    s = stack<int64_t>();
+    s.push(0);
+    for (int64_t i = 1; i <= n; i++)
+    {
+        d[i] = v[i] + b[f[i]];
+        b[i] = b[s.top()];
+        while (h[s.top()] < h[i])
+        {
+            b[i] = max(b[i], d[s.top()]);
+            s.pop();
+        }
+        if (h[s.top()] == h[i])
+            b[i] = max(b[i], b[s.top()]);
+        s.push(i);
+        r = max(r, d[i]);
+    }
+    cout << r << '\n';
+    return 0;
+}
