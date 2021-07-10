ff5aee0f1d1d8cc4efd9e7f3b3e85377834a6a15
nevikw39 <nevik.w39@gmail.com>
2021-07-10 17:51:33 +0800

    P-8-1

---
+++ ap325/d102.cpp
@@ -0,0 +1,71 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#ifndef nevikw39
+#define nevikw39      \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct _cerr
+{
+    template <typename T>
+    _cerr &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+void dfs(const vector<vector<int>> &t, vector<int> &a, int p, int x)
+{
+    a.push_back(x);
+    for (const auto &y : t[x])
+        if (y != p)
+        {
+            dfs(t, a, x, y);
+            a.push_back(x);
+        }
+}
+
+int main()
+{
+    nevikw39;
+    int n, r = 0;
+    cin >> n;
+    vector<vector<int>> t(n);
+    for (int i = 1; i < n; i++)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        t[a].push_back(b);
+        t[b].push_back(a);
+        r += w;
+    }
+    vector<int> a;
+    for (auto &i : t)
+        sort(ALL(i));
+    dfs(t, a, -1, 0);
+    cout << (r << 1) << '\n';
+    copy(ALL(a), ostream_iterator<int>(cout, " "));
+    return 0;
+}
