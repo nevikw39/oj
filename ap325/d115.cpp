67a88f8dee96d0deb73385005dfd89af08618b7f
nevikw39 <nevik.w39@gmail.com>
2021-07-12 18:17:53 +0800

    Q-8-15

---
+++ ap325/d115.cpp
@@ -0,0 +1,68 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#ifndef nevikw39
+#define nevikw39      \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct _cerr
+{
+    template <typename T>
+    _cerr &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+void dfs(const vector<vector<pair<int, int>>> &t, vector<int> &h, int &d, int x, int p)
+{
+    for (const auto &i : t[x])
+        if (i.ST != p)
+        {
+            dfs(t, h, d, i.ST, x);
+            d = max(d, h[x] + i.ND + h[i.ST]);
+            h[x] = max(h[x], i.ND + h[i.ST]);
+        }
+}
+
+int main()
+{
+    nevikw39;
+    int n, r = 0, d = 0;
+    cin >> n;
+    vector<vector<pair<int, int>>> t(n);
+    vector<int> h(n);
+    for (int i = 1; i < n; i++)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        t[a].emplace_back(b, w);
+        t[b].emplace_back(a, w);
+        r += w << 1;
+    }
+    dfs(t, h, d, 0, -1);
+    cout << r - d << '\n';
+    return 0;
+}
