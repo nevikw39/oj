c321778efd8a0e1d12da7ec466ac88745315022a
nevikw39 <nevik.w39@gmail.com>
2021-08-11 16:28:47 +0800

    Movie Festival

---
+++ CSES/1629_movie_festival.cpp
@@ -0,0 +1,63 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+struct node
+{
+    int64_t a, b;
+    constexpr bool operator<(const node &x) const
+    {
+        return b < x.b;
+    }
+};
+
+int main()
+{
+    nevikw39;
+    int64_t n, r = 0;
+    cin >> n;
+    vector<node> v(n);
+    for (auto &i : v)
+        cin >> i.a >> i.b;
+    sort(ALL(v));
+    n = 0;
+    for (const auto &i : v)
+        if (n <= i.a)
+        {
+            n = i.b;
+            ++r;
+        }
+    cout << r << '\n';
+    return 0;
+}
