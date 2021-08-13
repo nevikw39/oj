e091ec0534057ede859bcc2b2af16131c95c058b
nevikw39 <nevik.w39@gmail.com>
2021-08-13 19:02:41 +0800

    Room Allocation

---
+++ CSES/1164_room_allocation.cpp
@@ -0,0 +1,75 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+struct node
+{
+    int a, b, i;
+    constexpr bool operator<(const node &x) const
+    {
+        return b > x.b;
+    }
+};
+
+int main()
+{
+    nevikw39;
+    int64_t n, k = 0;
+    cin >> n;
+    vector<node> v(n);
+    vector<int64_t> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].a >> v[i].b;
+        v[i].i = i;
+    }
+    sort(ALL(v), [](const node &l, const node &r)
+         { return l.a < r.a; });
+    _heap<node> pq;
+    for (const node &i : v)
+    {
+        if (pq.size() && pq.top().b < i.a)
+        {
+            a[i.i] = a[pq.top().i];
+            pq.pop();
+        }
+        else
+            a[i.i] = ++k;
+        pq.push(i);
+    }
+    cout << k << '\n';
+    for (const auto &i : a)
+        cout << i << ' ';
+    return 0;
+}
