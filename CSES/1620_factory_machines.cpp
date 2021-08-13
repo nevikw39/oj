71a7909360365b663735aee71d8761bf2ca6b49f
nevikw39 <nevik.w39@gmail.com>
2021-08-13 19:33:14 +0800

    Factory Machines

---
+++ CSES/1620_factory_machines.cpp
@@ -0,0 +1,56 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int64_t n, t, r = 0;
+    cin >> n >> t;
+    vector<int64_t> k(n);
+    for (auto &i : k)
+        cin >> i;
+    auto f = [&](int64_t r)
+    {
+        __int128_t s = 0;
+        for (const auto &i : k)
+            s += r / i;
+        return s >= t;
+    };
+    for (int64_t jump = INT64_MAX >> 2; jump; jump >>= 1)
+        while (r + jump < (INT64_MAX >> 1) && !f(r + jump))
+            r += jump;
+    cout << r + 1 << '\n';
+    return 0;
+}
