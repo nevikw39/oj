849f566894a623bb6d5dbd53cde22f4e982fb67c
nevikw39 <nevik.w39@gmail.com>
2021-08-14 20:28:28 +0800

    AtCoder Beginner Contest 214: p. C

---
+++ AtCoder/Beginner_214/c.cpp
@@ -0,0 +1,56 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <atcoder/all>
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int64_t n, mn = 0;
+    cin >> n;
+    vector<int64_t> s(n), t(n);
+    for (auto &i : s)
+        cin >> i;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> t[i];
+        if (t[mn] > t[i])
+            mn = i;
+    }
+    for (int i = 0; i < n; i++)
+        t[(mn + i) % n] = min(t[(mn + i) % n], t[(mn + i - 1 + n) % n] + s[(mn + i - 1 + n) % n]);
+    for (const auto &i : t)
+        cout << i << '\n';
+    return 0;
+}
