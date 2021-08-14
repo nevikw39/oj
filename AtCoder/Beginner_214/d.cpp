13459c1255e2195f612ea406af8aaf863ff4430a
nevikw39 <nevik.w39@gmail.com>
2021-08-14 22:03:21 +0800

    AtCoder Beginner Contest 214: p. D

---
+++ AtCoder/Beginner_214/d.cpp
@@ -0,0 +1,68 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <atcoder/all>
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+struct edge
+{
+    int64_t u, v, w;
+    constexpr bool operator<(const edge &x) const
+    {
+        return w < x.w;
+    }
+};
+
+int main()
+{
+    nevikw39;
+    int64_t n, r = 0;
+    cin >> n;
+    vector<edge> v(n - 1);
+    for (edge &i : v)
+    {
+        cin >> i.u >> i.v >> i.w;
+        --i.u;
+        --i.v;
+    }
+    sort(ALL(v));
+    dsu ds(n);
+    for (const edge &i : v)
+    {
+        r += i.w * ds.size(i.u) * ds.size(i.v);
+        ds.merge(i.u, i.v);
+    }
+    cout << r << '\n';
+    return 0;
+}
