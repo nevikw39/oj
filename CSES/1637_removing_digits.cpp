7b974f358dc2613020a52567287d2ddecb15a20e
nevikw39 <nevik.w39@gmail.com>
2021-08-19 15:28:19 +0800

    Removing Digits

---
+++ CSES/1637_removing_digits.cpp
@@ -0,0 +1,62 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+template <typename T = int64_t>
+T inline bwmin(T x, T y)
+{
+    return y ^ ((x ^ y) & -(x < y));
+}
+
+int64_t dp(int64_t n)
+{
+    static vector<int64_t> DP(n + 1, INT64_MAX >> 1);
+    if (n < 10)
+        return !!n;
+    if (DP[n] != INT64_MAX >> 1)
+        return DP[n];
+    for (int i = n; i; i /= 10)
+        if (i % 10)
+            DP[n] = bwmin(DP[n], dp(n - i % 10) + 1);
+    return DP[n];
+}
+
+int main()
+{
+    nevikw39;
+    int64_t n;
+    cin >> n;
+    cout << dp(n) << '\n';
+    return 0;
+}
