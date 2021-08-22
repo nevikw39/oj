fc38de7bcd36de3bd3a0e64c0506191ffff46b74
nevikw39 <nevikw39@icloud.com>
2021-08-22 13:39:41 +0800

    code templ

--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -4,9 +4,8 @@
  * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
  * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
  **/
-#include <bits/extc++.h>
 #ifndef nevikw39
-#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#define __builtin_sprintf sprintf
 #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
 #pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
 #pragma comment(linker, "/stack:200000000")
@@ -16,10 +15,9 @@ struct
     auto &operator<<(const T &x) { return *this; }
 } __cerr;
 #define cerr __cerr
-#else
-#pragma message("hello, nevikw39")
 #endif
 #pragma message("GL; HF!")
+#include <bits/extc++.h>
 #define ALL(X) begin(X), end(X)
 #define ST first
 #define ND second
@@ -28,13 +26,20 @@ using namespace __gnu_cxx;
 using namespace __gnu_pbds;
 template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
 using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
-template <typename K, typename M = null_type>
-using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
 template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
 using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
 
+class Solution
+{
+public:
+};
+
+#ifdef nevikw39
+#pragma message("hello, nevikw39")
 int main()
 {
-    nevikw39;
-    return 0;
+    Solution sln;
 }
+#endif
