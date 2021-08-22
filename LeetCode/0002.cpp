c98ad50c5980500f34b4ecafc6b347d83b34650d
nevikw39 <nevikw39@icloud.com>
2021-08-23 01:00:08 +0800

    LeetCode 0002: Add Two Numbers

---
+++ LeetCode/0002.cpp
@@ -0,0 +1,82 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#ifndef nevikw39
+#define __builtin_sprintf sprintf
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+#ifdef nevikw39
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+#endif
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *front = nullptr, *ptr;
+        int carry = 0;
+        while (l1 || l2)
+        {
+            ptr = (front ? ptr->next : front) = new ListNode((l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry);
+            carry = ptr->val / 10;
+            ptr->val %= 10;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
+        }
+        if (carry)
+            ptr->next = new ListNode(carry);
+        return front;
+    }
+};
+
+#ifdef nevikw39
+#pragma message("hello, nevikw39")
+int main()
+{
+    Solution sln;
+    for (auto ptr = sln.addTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))), new ListNode(5, new ListNode(6, new ListNode(4)))); ptr; ptr = ptr->next)
+        cout << ptr->val << ' ';
+    cout << '\n';
+    for (auto ptr = sln.addTwoNumbers(new ListNode(0), new ListNode(0)); ptr; ptr = ptr->next)
+        cout << ptr->val << ' ';
+    cout << '\n';
+    for (auto ptr = sln.addTwoNumbers(new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))), new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))); ptr; ptr = ptr->next)
+        cout << ptr->val << ' ';
+    cout << '\n';
+    return 0;
+}
+#endif
