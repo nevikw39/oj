2654b8df045753216a648bdf21431f7a7d9a3545
nevikw39 <nevik.w39@gmail.com>
2021-08-29 10:39:24 +0800

    LeetCode 5854. Minimum Difference Between Highest and Lowest of K Scores

---
+++ LeetCode/5854.cpp
@@ -0,0 +1,10 @@
+#include "5854.hpp"
+
+int main()
+{
+    vector<int> a = {90}, b = {9, 4, 1, 7};
+    Solution sln;
+    cout << sln.minimumDifference(a, 1) << '\n'
+         << sln.minimumDifference(b, 2) << '\n';
+    return 0;
+}
