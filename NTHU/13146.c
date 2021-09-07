04be5d073008ad9d6899112f5a06b0731637a859
nevikw39 <nevikw39@icloud.com>
2021-09-07 17:47:52 +0800

    NTHU OJ 13146 - Carrying Hotpot

---
+++ NTHU/13146.c
@@ -0,0 +1,27 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <math.h>
+#include <stdio.h>
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    double x, y, p;
+    int z, n = 1;
+    scanf("%lf%lf%d", &x, &y, &z);
+    for (int i = 0; i < z; i++)
+        n *= 10;
+    printf("%.*lf\n", z, round(x * y * n) / n);
+    return 0;
+}
