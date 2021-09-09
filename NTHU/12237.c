4dbc2ccb89780d135a084b5109e676b03ba72c8d
nevikw39 <nevikw39@icloud.com>
2021-09-09 19:59:59 +0800

    NTHU OJ 12237 - TA's Heartfelt

---
+++ NTHU/12237.c
@@ -0,0 +1,35 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+typedef union
+{
+    float f;
+    unsigned int n;
+} U;
+
+int main()
+{
+    U u;
+    while (~scanf("%f", &u.f))
+    {
+        for (int i = 31; i >= 0; i--)
+            putchar_unlocked('0' + !!(u.n & (1U << i)));
+        putchar_unlocked('\n');
+    }
+    return 0;
+}
