c2a07b6905f3ec736c30ece1b484a9cdd35c105c
nevikw39 <nevikw39@icloud.com>
2021-09-10 12:40:33 +0800

    ZeroJudge c143

---
+++ ZeroJudge/c143.cpp
@@ -0,0 +1,72 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int n, m, t, r = 0;
+    cin >> n >> m >> t;
+    vector<vector<int>> v(n + 1, vector<int>(m + 1));
+    while (t--)
+    {
+        int x1, y1, x2, y2, dx, dy, d;
+        cin >> x1 >> y1 >> x2 >> y2;
+        d = max(abs(x1 - x2), abs(y1 - y2));
+        if (x1 < x2)
+            dx = 1;
+        else if (x1 == x2)
+            dx = 0;
+        else
+            dx = -1;
+        if (y1 < y2)
+            dy = 1;
+        else if (y1 == y2)
+            dy = 0;
+        else
+            dy = -1;
+        while (d-- >= 0)
+        {
+            if (!v[x1][y1])
+            {
+                v[x1][y1] = true;
+                ++r;
+            }
+            x1 += dx;
+            y1 += dy;
+        }
+    }
+    cout << r << '\n';
+    return 0;
+}
