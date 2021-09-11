94d51ecf444f4c0921b8074c192ea2d4615b7248
nevikw39 <nevik.w39@gmail.com>
2021-09-11 19:49:40 +0800

    auto open in vs code

--- new.sh
+++ new.sh
@@ -7,11 +7,14 @@ case $1 in
     AtCoder)
         mkdir "AtCoder/$2";
         cp AtCoder/AtCoder.cpp "AtCoder/$2/a.cpp";
+        code "AtCoder/$2/a.cpp";
         ;;
     LeetCode)
         cp LeetCode/LeetCode.cpp "LeetCode/$2.cpp";
         sed -i "s/LeetCode/$2/" "LeetCode/$2.cpp";
         cp LeetCode/LeetCode.hpp "LeetCode/$2.hpp";
+        code "LeetCode/$2.cpp";
+        code "LeetCode/$2.hpp";
         ;;
     *)
         echo "Value Error: Not supported OJ.";
