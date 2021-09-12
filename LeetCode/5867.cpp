01567f6b896a0466a9ac6d0cd03c3f7e9f29bbe6
nevikw39 <nevik.w39@gmail.com>
2021-09-12 10:37:25 +0800

    LeetCode 5867. Reverse Prefix of Word

---
+++ LeetCode/5867.cpp
@@ -0,0 +1,10 @@
+#include "5867.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.reversePrefix("abcdefd", 'd') << '\n'
+         << sln.reversePrefix("xyxzxe", 'z') << '\n'
+         << sln.reversePrefix("abcd", 'z') << '\n';
+    return 0;
+}
