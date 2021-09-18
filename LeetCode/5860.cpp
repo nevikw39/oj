6de413a1ba07868e0084e22aeff1403640e32af3
nevikw39 <nevikw39@icloud.com>
2021-09-18 22:54:21 +0800

    LeetCode 5860. Find Original Array From Doubled Array

---
+++ LeetCode/5860.cpp
@@ -0,0 +1,17 @@
+#include "5860.hpp"
+
+int main()
+{
+    vector<int> a = {1, 3, 4, 2, 6, 8}, b = {6, 3, 0, 1}, c = {1};
+    Solution sln;
+    for (const int &i : sln.findOriginalArray(a))
+        cout << i << '\n';
+    cout << '\n';
+    for (const int &i : sln.findOriginalArray(b))
+        cout << i << '\n';
+    cout << '\n';
+    for (const int &i : sln.findOriginalArray(c))
+        cout << i << '\n';
+    cout << '\n';
+    return 0;
+}
