bfd8dcfc955f1fa138a7f6ca5f85f4ee83024d05
nevikw39 <nevikw39@icloud.com>
2021-09-19 10:35:13 +0800

    LeetCode 5875. Final Value of Variable After Performing Operations

---
+++ LeetCode/5875.cpp
@@ -0,0 +1,11 @@
+#include "5875.hpp"
+
+int main()
+{
+    vector<string> a = {"--X", "X++", "X++"}, b = {"++X", "++X", "X++"}, c = {"X++", "++X", "--X", "X--"};
+    Solution sln;
+    cout << sln.finalValueAfterOperations(a) << '\n'
+         << sln.finalValueAfterOperations(b) << '\n'
+         << sln.finalValueAfterOperations(c) << '\n';
+    return 0;
+}
