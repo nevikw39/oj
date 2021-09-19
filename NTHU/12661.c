8b19b242cad5e02b1a91f18e9844099e954c6d42
nevikw39 <nevikw39@icloud.com>
2021-09-19 17:31:59 +0800

    NTHU OJ 12661 - The night's watch

--- NTHU/12661.c
+++ NTHU/12661.c
@@ -4,12 +4,10 @@
  * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
  * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
  **/
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #ifndef nevikw39
-#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
-#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
-#pragma comment(linker, "/stack:200000000")
 #else
 #pragma message("hello, nevikw39")
 #endif
@@ -20,24 +18,31 @@ int inline static max(int l, int r)
     return l ^ ((l ^ r) & -(l < r));
 }
 
+int inline static min(int l, int r)
+{
+    return r ^ ((l ^ r) & -(l < r));
+}
+
 int main()
 {
     int t;
     scanf("%d", &t);
     while (t--)
     {
-        int n, m, k, *a, *head, *tail;
+        int n, m, k, *a, r = INT_MIN;
         scanf("%d%d%d", &n, &m, &k);
         a = malloc(sizeof(int) * n);
-        head = a, tail = a + n - 1;
         for (int i = 0; i < n; i++)
             scanf("%d", a + i);
-        for (int i = 0; i < m - 1; i++)
-            if (i < k && *head < *tail || i >= k && *head > *tail)
-                ++head;
-            else
-                --tail;
-        printf("%d\n", max(*head, *tail));
+        k = min(k, m - 1);
+        for (int i = 0; i <= k; i++)
+        {
+            int t = INT_MAX;
+            for (int j = 0; j <= m - 1 - k; j++)
+                t = min(t, max(a[i + j], a[n - m + i + j]));
+            r = max(r, t);
+        }
+        printf("%d\n", r);
         free(a);
     }
     return 0;
