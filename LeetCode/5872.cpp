1d56df0f7e9680b747d1b05ee5593b06f009986e
nevikw39 <nevikw39@icloud.com>
2021-10-02 22:49:07 +0800

    5872. Number of Pairs of Strings With Concatenation Equal to Target

---
+++ LeetCode/5872.cpp
@@ -0,0 +1,11 @@
+#include "5872.hpp"
+
+int main()
+{
+    vector<string> a = {"777", "7", "77", "77"}, b = {"123", "4", "12", "34"}, c = {"1", "1", "1"};
+    Solution sln;
+    cout << sln.numOfPairs(a, "7777") << '\n'
+         << sln.numOfPairs(b, "1234") << '\n'
+         << sln.numOfPairs(c, "11") << '\n';
+    return 0;
+}
