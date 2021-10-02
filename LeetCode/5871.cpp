57102e13eb0285892285b60db37a75ca4f23ef32
nevikw39 <nevikw39@icloud.com>
2021-10-02 22:42:14 +0800

    5871. Convert 1D Array Into 2D Array

---
+++ LeetCode/5871.cpp
@@ -0,0 +1,32 @@
+#include "5871.hpp"
+
+int main()
+{
+    vector<int> a = {1, 2, 3, 4}, b = {1, 2, 3}, c = {1, 2}, d = {3};
+    Solution sln;
+    for (const auto &i : sln.construct2DArray(a, 2, 2))
+    {
+        for (const int &j : i)
+            cout << j << ' ';
+        cout << '\n';
+    }
+    for (const auto &i : sln.construct2DArray(b, 1, 3))
+    {
+        for (const int &j : i)
+            cout << j << ' ';
+        cout << '\n';
+    }
+    for (const auto &i : sln.construct2DArray(c, 1, 1))
+    {
+        for (const int &j : i)
+            cout << j << ' ';
+        cout << '\n';
+    }
+    for (const auto &i : sln.construct2DArray(d, 1, 2))
+    {
+        for (const int &j : i)
+            cout << j << ' ';
+        cout << '\n';
+    }
+    return 0;
+}
