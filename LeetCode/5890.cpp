57aee39aeb97448ba3fe5cb6b1ab777fd8a9d773
nevikw39 <nevikw39@icloud.com>
2021-10-03 10:36:52 +0800

    5890. Minimum Moves to Convert String

---
+++ LeetCode/5890.cpp
@@ -0,0 +1,10 @@
+#include "5890.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.minimumMoves("XXX") << '\n'
+         << sln.minimumMoves("XXOX") << '\n'
+         << sln.minimumMoves("OOOO") << '\n';
+    return 0;
+}
