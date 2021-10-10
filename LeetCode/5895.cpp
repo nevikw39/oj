173dfc89a1ce3a726d2c5835ce8f5d77da5e1771
nevikw39 <nevik.w39@gmail.com>
2021-10-10 10:42:02 +0800

    5895. Minimum Operations to Make a Uni-Value Grid

---
+++ LeetCode/5895.cpp
@@ -0,0 +1,11 @@
+#include "5895.hpp"
+
+int main()
+{
+    vector<vector<int>> a = {{2, 4}, {6, 8}}, b = {{2, 1}, {5, 3}}, c = {{1, 2}, {3, 4}};
+    Solution sln;
+    cout << sln.minOperations(a, 2) << '\n'
+         << sln.minOperations(b, 1) << '\n'
+         << sln.minOperations(c, 2) << '\n';
+    return 0;
+}
