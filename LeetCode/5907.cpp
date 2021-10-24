7a43b22a7d84e033df362fa19da0aa3cf46d1962
nevikw39 <nevik.w39@gmail.com>
2021-10-24 10:48:11 +0800

    5907. Next Greater Numerically Balanced Number

---
+++ LeetCode/5907.cpp
@@ -0,0 +1,11 @@
+#include "5907.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.nextBeautifulNumber(10) << '\n'
+         << sln.nextBeautifulNumber(1000) << '\n'
+         << sln.nextBeautifulNumber(3000) << '\n'
+         << sln.nextBeautifulNumber(1000000) << '\n';
+    return 0;
+}
