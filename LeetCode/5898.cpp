40241742597dbe4d1da6fe7b8aecd0b6282f94d5
nevikw39 <nevikw39@icloud.com>
2021-10-30 22:44:09 +0800

    5898. Kth Distinct String in an Array

---
+++ LeetCode/5898.cpp
@@ -0,0 +1,11 @@
+#include "5898.hpp"
+
+int main()
+{
+    vector<string> a = {"d", "b", "c", "b", "c", "a"}, b = {"aaa", "aa", "a"}, c = {"a", "b", "a"};
+    Solution sln;
+    cout << sln.kthDistinct(a, 2) << '\n'
+         << sln.kthDistinct(b, 1) << '\n'
+         << sln.kthDistinct(c, 3) << '\n';
+    return 0;
+}
