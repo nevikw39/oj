3535cf2ec04c2fbba51a9769e118dac09ecf26bc
nevikw39 <nevikw39@icloud.com>
2021-10-31 10:36:52 +0800

    5914. Smallest Index With Equal Value

---
+++ LeetCode/5914.cpp
@@ -0,0 +1,12 @@
+#include "5914.hpp"
+
+int main()
+{
+    vector<int> a = {0, 1, 2}, b = {4, 3, 2, 1}, c = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, d = {2, 1, 3, 5, 2};
+    Solution sln;
+    cout << sln.smallestEqual(a) << '\n'
+         << sln.smallestEqual(b) << '\n'
+         << sln.smallestEqual(c) << '\n'
+         << sln.smallestEqual(d) << '\n';
+    return 0;
+}
