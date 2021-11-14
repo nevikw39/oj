66b16f007c5b2236c8ad5c8885c2c243aaaacb13
nevikw39 <nevik.w39@gmail.com>
2021-11-14 10:40:06 +0800

    5926. Time Needed to Buy Tickets

---
+++ LeetCode/5926.cpp
@@ -0,0 +1,10 @@
+#include "5926.hpp"
+
+int main()
+{
+    vector<int> a = {2, 3, 2}, b = {5, 1, 1, 1};
+    Solution sln;
+    cout << sln.timeRequiredToBuy(a, 2) << '\n'
+         << sln.timeRequiredToBuy(b, 0) << '\n';
+    return 0;
+}
