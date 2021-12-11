096713334fe6ea30a1f56a2f4c7e2d7191e5d56f
nevikw39 <nevikw39@icloud.com>
2021-12-11 22:50:12 +0800

    5934. Find Subsequence of Length K With the Largest Sum

---
+++ LeetCode/5934.cpp
@@ -0,0 +1,20 @@
+#include "5934.hpp"
+
+int main()
+{
+    vector<int> a = {2, 1, 3, 3}, b = {-1, -2, 3, 4}, c = {3, 4, 3, 3}, d = {50, -75};
+    Solution sln;
+    for (auto &i : sln.maxSubsequence(a, 2))
+        cout << i << ' ';
+    cout << '\n';
+    for (auto &i : sln.maxSubsequence(b, 3))
+        cout << i << ' ';
+    cout << '\n';
+    for (auto &i : sln.maxSubsequence(c, 2))
+        cout << i << ' ';
+    cout << '\n';
+    for (auto &i : sln.maxSubsequence(d, 2))
+        cout << i << ' ';
+    cout << '\n';
+    return 0;
+}
