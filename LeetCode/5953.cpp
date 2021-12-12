a66946bd43ce46e7808c6fe88bc6ebe593f23d5c
nevikw39 <nevikw39@icloud.com>
2021-12-12 11:21:42 +0800

    5953. Sum of Subarray Ranges

---
+++ LeetCode/5953.cpp
@@ -0,0 +1,11 @@
+#include "5953.hpp"
+
+int main()
+{
+    vector<int> a = {1, 2, 3}, b = {1, 3, 3}, c = {4, -2, -3, 4, 1};
+    Solution sln;
+    cout << sln.subArrayRanges(a) << '\n'
+         << sln.subArrayRanges(b) << '\n'
+         << sln.subArrayRanges(c) << '\n';
+    return 0;
+}
