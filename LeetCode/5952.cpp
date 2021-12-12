077fd0abc44be562a661a5aeda62882b58fa7fc4
nevikw39 <nevikw39@icloud.com>
2021-12-12 10:38:58 +0800

    5952. Rings and Rods

---
+++ LeetCode/5952.cpp
@@ -0,0 +1,10 @@
+#include "5952.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.countPoints("B0B6G0R6R0R6G9") << '\n'
+         << sln.countPoints("B0R0G0R9R0B0G0") << '\n'
+         << sln.countPoints("G4") << '\n';
+    return 0;
+}
