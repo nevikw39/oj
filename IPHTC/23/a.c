2b5665358ac587423e80181be8861b6789dcfd94
nevikw39 <nevikw39@icloud.com>
2021-12-24 03:10:08 +0800

    Skate Shoes Sliding

---
+++ IPHTC/23/a.c
@@ -0,0 +1,27 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n + 1);
+    return 0;
+}
