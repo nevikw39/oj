995b97595b6f3c9585c86e3929f81385c5c53b7a
nevikw39 <nevikw39@icloud.com>
2021-12-25 23:07:13 +0800

    5947. Find All Possible Recipes from Given Supplies

---
+++ LeetCode/5947.cpp
@@ -0,0 +1,15 @@
+#include "5947.hpp"
+
+int main()
+{
+    vector<string> a0 = {"bread"}, b0 = {"ju", "fzjnm", "x", "e", "zpmcz", "h", "q"}, a2 = {"yeast"}, b2 = {"f", "hveml", "cpivl", "d"};
+    vector<vector<string>> a1 = {{"yeast", "flour"}}, b1 = {{"d"}, {"hveml", "f", "cpivl"}, {"cpivl", "zpmcz", "h", "e", "fzjnm", "ju"}, {"cpivl", "hveml", "zpmcz", "ju", "h"}, {"h", "fzjnm", "e", "q", "x"}, {"d", "hveml", "cpivl", "q", "zpmcz", "ju", "e", "x"}, {"f", "hveml", "cpivl"}};
+    Solution sln;
+    for (auto &&i : sln.findAllRecipes(a0, a1, a2))
+        cout << i << '\n';
+    cout << '\n';
+    for (auto &&i : sln.findAllRecipes(b0, b1, b2))
+        cout << i << '\n';
+    cout << '\n';
+    return 0;
+}
