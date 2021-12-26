63349f421f882ad085275165b8dbfaaf94c2abe8
nevikw39 <nevikw39@icloud.com>
2021-12-26 11:38:44 +0800

    5965. Intervals Between Identical Elements

---
+++ LeetCode/59645.cpp
@@ -0,0 +1,14 @@
+#include "59645.hpp"
+
+int main()
+{
+    vector<int> a = {2, 1, 3, 1, 2, 3, 3}, b = {10, 5, 10, 10};
+    Solution sln;
+    for (auto &&i : sln.getDistances(a))
+        cout << i << '\n';
+    cout << '\n';
+    for (auto &&i : sln.getDistances(b))
+        cout << i << '\n';
+    cout << '\n';
+    return 0;
+}
