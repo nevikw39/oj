035c512cb1dcc93c750f8a3bfc1da75603ed7e0d
nevikw39 <nevik.w39@gmail.com>
2022-01-02 10:42:18 +0800

    5968. Number of Laser Beams in a Bank

---
+++ LeetCode/5968.cpp
@@ -0,0 +1,10 @@
+#include "5968.hpp"
+
+int main()
+{
+    vector<string> a = {"011001", "000000", "010100", "001000"}, b = {"000", "111", "000"};
+    Solution sln;
+    cout << sln.numberOfBeams(a) << '\n'
+         << sln.numberOfBeams(b) << '\n';
+    return 0;
+}
