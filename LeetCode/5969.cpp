b929c0331f9be05cc790c7c984d2801ae80ef70d
nevikw39 <nevik.w39@gmail.com>
2022-01-02 10:57:55 +0800

    5969. Destroying Asteroids

---
+++ LeetCode/5969.cpp
@@ -0,0 +1,11 @@
+#include "5969.hpp"
+
+int main()
+{
+    vector<int> a = {3, 9, 19, 5, 21}, b = {4, 9, 23, 4}, c = {1};
+    Solution sln;
+    cout << sln.asteroidsDestroyed(10, a) << '\n'
+         << sln.asteroidsDestroyed(5, b) << '\n'
+         << sln.asteroidsDestroyed(1, c) << '\n';
+    return 0;
+}
