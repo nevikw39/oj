33ae904e82c0c9b6369c06231f87b8d67f2eb080
nevikw39 <nevik.w39@gmail.com>
2022-01-02 10:34:45 +0800

    5967. Check if All A's Appears Before All B's

---
+++ LeetCode/5967.cpp
@@ -0,0 +1,10 @@
+#include "5967.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.checkString("aaabbb") << '\n'
+         << sln.checkString("abab") << '\n'
+         << sln.checkString("bbb") << '\n';
+    return 0;
+}
