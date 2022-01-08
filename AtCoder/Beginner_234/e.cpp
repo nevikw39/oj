d601c0179bfb254bb4b80195b601fa015d5212b8
nevikw39 <nevikw39@icloud.com>
2022-01-08 20:47:07 +0800

    AtCoder Beginner Contest 234 E

---
+++ AtCoder/Beginner_234/e.cpp
@@ -0,0 +1,67 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <atcoder/all>
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    set<int64_t> a;
+    for (int d = 0; d < 10; d++)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            string s;
+            for (int j = 0; j < 18 && i + d * j < 10; j++)
+            {
+                s.push_back('0' + i + d * j);
+                a.insert(stoll(s));
+            }
+        }
+        for (int i = 9; i >= 0; i--)
+        {
+            string s;
+            for (int j = 0; j < 18 && i - d * j >= 0; j++)
+            {
+                s.push_back('0' + i - d * j);
+                a.insert(stoll(s));
+            }
+        }
+    }
+    int64_t x;
+    cin >> x;
+    cout << *a.lower_bound(x) << '\n';
+    return 0;
+}
