3601e1549c27d8ad899d22dfdf3abb7fca0f94e2
nevikw39 <nevik.w39@gmail.com>
2022-01-22 23:12:05 +0800

    Count the Hidden Sequences

---
+++ LeetCode/5972.cpp
@@ -0,0 +1,13 @@
+#include "5972.hpp"
+
+int main()
+{
+    vector<int> a = {1, -3, 4}, b = {3, -4, 5, 1, -2}, c = {4, -7, 2}, d = {-40}, e = {83702, -5216};
+    Solution sln;
+    cout << sln.numberOfArrays(a, 1, 6) << '\n'
+         << sln.numberOfArrays(b, -4, 5) << '\n'
+         << sln.numberOfArrays(c, 3, 6) << '\n'
+         << sln.numberOfArrays(d, -46, 53) << '\n'
+         << sln.numberOfArrays(e, -82788, 14602) << '\n';
+    return 0;
+}
---
+++ LeetCode/5972.hpp
@@ -0,0 +1,53 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+class Solution
+{
+public:
+    int numberOfArrays(vector<int> &differences, int lower, int upper)
+    {
+        int64_t mn = 0, mx = 0;
+        vector<int64_t> v(ALL(differences));
+        for (int i = 0, n = v.size(); i < n; i++)
+        {
+            v[i] += i ? v[i - 1] : 0;
+            mn = min(mn, v[i]);
+            mx = max(mx, v[i]);
+        }
+        if (mn == mx)
+            return max(upper - lower + mn + 1, int64_t(0));
+        return max((upper - lower) - (mx - mn) + 1, int64_t(0));
+    }
+};
