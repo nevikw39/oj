039ffc5a790a2f9e807e4ae26152d98a8b18b422
nevikw39 <nevik.w39@gmail.com>
2022-01-22 22:36:31 +0800

    Minimum Cost of Buying Candies With Discount

---
+++ LeetCode/5971.cpp
@@ -0,0 +1,11 @@
+#include "5971.hpp"
+
+int main()
+{
+    vector<int> a = {1, 2, 3}, b = {6, 5, 7, 9, 2, 2}, c = {5, 5};
+    Solution sln;
+    cout << sln.minimumCost(a) << '\n'
+         << sln.minimumCost(b) << '\n'
+         << sln.minimumCost(c) << '\n';
+    return 0;
+}
