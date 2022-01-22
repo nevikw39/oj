431cc6bb2f1a25146085c4ff948ab2267c43e357
nevikw39 <nevik.w39@gmail.com>
2022-01-22 13:44:43 +0800

    Stone Game IV

---
+++ LeetCode/1510.cpp
@@ -0,0 +1,10 @@
+#include "1510.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.winnerSquareGame(1) << '\n'
+         << sln.winnerSquareGame(2) << '\n'
+         << sln.winnerSquareGame(4) << '\n';
+    return 0;
+}
