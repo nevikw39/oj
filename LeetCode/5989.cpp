7ebe8a1e6ca975578bf1f1f279dfe5e09980369a
nevikw39 <nevik.w39@gmail.com>
2022-01-23 10:38:23 +0800

    Count Elements With Strictly Smaller and Greater Elements

---
+++ LeetCode/5989.cpp
@@ -0,0 +1,11 @@
+#include "5989.hpp"
+
+int main()
+{
+    vector<int> a = {11, 7, 2, 15}, b = {-3, 3, 3, 90}, c = {0};
+    Solution sln;
+    cout << sln.countElements(a) << '\n'
+         << sln.countElements(b) << '\n'
+         << sln.countElements(c) << '\n';
+    return 0;
+}
