4cb785ee00c1bcbb99e8a9e3ba3514070d22a8af
nevikw39 <nevik.w39@gmail.com>
2022-01-25 20:10:15 +0800

    A - H0-0

---
+++ HPS_A/a.c
@@ -0,0 +1,36 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("tune=native")
+#pragma comment(linker, "/stack:200000000")
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int s, d;
+        scanf("%d%d", &s, &d);
+        if ((s & 1) == (d & 1) && s >= d)
+            printf("%d %d\n", (s & d) + ((s ^ d) >> 1), s - d >> 1);
+        else
+            puts("impossible");
+    }
+    return 0;
+}
