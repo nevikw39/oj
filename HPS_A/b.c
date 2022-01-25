c31a6705be06d8ddd1aef920b5dc53ef6fd2d2f9
nevikw39 <nevik.w39@gmail.com>
2022-01-25 20:10:25 +0800

    B - H0-1

---
+++ HPS_A/b.c
@@ -0,0 +1,34 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <inttypes.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("tune=native")
+#pragma comment(linker, "/stack:200000000")
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int h, m;
+    while (~scanf("%d:%d", &h, &m) && (h || m))
+    {
+        double a = fabs(h * 30 + m * 30.0 / 60 - m * 6);
+        if (a > 180)
+            a = 360 - a;
+        printf("%.3lf\n", a);
+    }
+    return 0;
+}
