8d80abea13ab509cfea318e25fd1505b1efd06bc
nevikw39 <nevik.w39@gmail.com>
2022-01-26 19:09:19 +0800

    A - H7-0 `Codeforces 1538F`

---
+++ HPS_B/a.c
@@ -0,0 +1,37 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("tune=native")
+#pragma comment(linker, "/stack:200000000")
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int l, r, a = 0;
+        scanf("%d%d", &l, &r);
+        for (; r; r /= 10)
+            a += r;
+        for (; l; l /= 10)
+            a -= l;
+        printf("%d\n", a);
+    }
+    return 0;
+}
