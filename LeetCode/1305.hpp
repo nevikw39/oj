7ae28902cad1bcd6d181bf10e4ee8b5ba6c811ce
nevikw39 <nevik.w39@gmail.com>
2022-01-26 13:35:41 +0800

    All Elements in Two Binary Search Trees

---
+++ LeetCode/1305.hpp
@@ -0,0 +1,63 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+class Solution
+{
+private:
+    vector<int> v1, v2;
+    void dfs(TreeNode *root, bool flag)
+    {
+        if (!root)
+            return;
+        dfs(root->left, flag);
+        if (flag)
+            v1.push_back(root->val);
+        else
+            v2.push_back(root->val);
+        dfs(root->right, flag);
+    }
+
+public:
+    vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+    {
+        v1.clear();
+        dfs(root1, true);
+        v2.clear();
+        dfs(root2, false);
+        vector<int> y;
+        merge(ALL(v1), ALL(v2), back_inserter(y));
+        return y;
+    }
+};
