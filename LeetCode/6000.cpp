4b51b08918a0658454cd7c00f814d0349db9f276
nevikw39 <nevik.w39@gmail.com>
2022-02-06 10:39:15 +0800

    Sort Even and Odd Indices Independently

---
+++ LeetCode/6000.cpp
@@ -0,0 +1,13 @@
+#include "6000.hpp"
+
+int main()
+{
+    vector<int> a = {4, 1, 2, 3}, b = {2, 1};
+    Solution sln;
+    for (auto &&i : sln.sortEvenOdd(a))
+        cout << i << '\n';
+    cout << '\n';
+    for (auto &&i : sln.sortEvenOdd(b))
+        cout << i << '\n';
+    return 0;
+}
