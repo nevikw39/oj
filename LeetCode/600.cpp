5cd3e8db051331a8088a3e18c8fe0b599e45cb2b
nevikw39 <nevik.w39@gmail.com>
2022-02-06 10:56:37 +0800

    Smallest Value of the Rearranged Number

---
+++ LeetCode/600.cpp
@@ -0,0 +1,10 @@
+#include "600.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.smallestNumber(310) << '\n'
+         << sln.smallestNumber(-7605) << '\n'
+         << sln.smallestNumber(5396980208411) << '\n';
+    return 0;
+}
