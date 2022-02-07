c641499ad2514afcaed8d3db0883ab2b3484178a
nevikw39 <nevikw39@icloud.com>
2022-02-07 19:52:13 +0800

    Find the Difference

---
+++ LeetCode/0389.cpp
@@ -0,0 +1,9 @@
+#include "0389.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.findTheDifference("abcd", "abcde") << '\n'
+         << sln.findTheDifference("", "y") << '\n';
+    return 0;
+}
