1291501adfef02f040849e3f40e778cc7a74c661
nevikw39 <nevik.w39@gmail.com>
2022-02-08 17:34:24 +0800

    Fibonacci Numbers

---
+++ CSES/1722_fibonacci_numbers.cpp
@@ -0,0 +1,60 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+struct M
+{
+    int64_t a = 0, b = 0, c = 0, d = 0;
+    M operator*(const M &r) { return {(a * r.a + b * r.c) % 1000000007, (a * r.b + b * r.d) % 1000000007, (c * r.a + d * r.c) % 1000000007, (c * r.b + d * r.d) % 1000000007}; }
+};
+
+pair<int64_t, int64_t> mul(const pair<int64_t, int64_t> &v, const M &m) { return {(v.first * m.a + v.second * m.c) % 1000000007, (v.first * m.b + v.second * m.d) % 1000000007}; }
+
+M binexp(M a, int64_t n)
+{
+    M y{1, 0, 0, 1};
+    for (; n > 0; a = a * a, n >>= 1)
+        if (n & 1)
+            y = y * a;
+    return y;
+}
+
+int main()
+{
+    nevikw39;
+    int64_t n;
+    cin >> n;
+    cout << mul({0, 1}, binexp({1, 1, 1, 0}, n)).ST << '\n';
+    return 0;
+}
