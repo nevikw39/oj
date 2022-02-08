8179a43e4cd1c055fd2b5f91cb1a2fd5cb1630a0
nevikw39 <nevik.w39@gmail.com>
2022-02-08 19:13:52 +0800

    AtCoder Beginner Contest 228 p. E

---
+++ Vjudge/HWTC_1/e.cpp
@@ -0,0 +1,47 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+constexpr int M = 998244353;
+
+int binexp(int64_t a, int64_t n)
+{
+    int y = 1;
+    for (a %= M; n; n >>= 1, a = a * a % M)
+        if (n & 1)
+            y = y * a % M;
+    return y;
+}
+
+int main()
+{
+    nevikw39;
+    int64_t n, k, m;
+    cin >> n >> k >> m;
+    cout << binexp(m, binexp(k, n)) << '\n';
+    return 0;
+}
