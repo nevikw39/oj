27f81a4c8b8cf9954dce8fc238c9269e3a47841f
nevikw39 <nevik.w39@gmail.com>
2022-02-09 14:13:26 +0800

    Codeforces  4C

---
+++ Vjudge/HWTC_2/c.cpp
@@ -0,0 +1,46 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int n;
+    cin >> n;
+    _hash<string, int> ht;
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        if (ht[s])
+            cout << s << ht[s] << '\n';
+        else
+            cout << "OK\n";
+        ++ht[s];
+    }
+    return 0;
+}
