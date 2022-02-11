f16ba02b4f7cba476822b1588181cdd4d8db94fb
nevikw39 <nevik.w39@gmail.com>
2022-02-11 10:39:40 +0800

    Permutation in String

---
+++ LeetCode/0567.cpp
@@ -0,0 +1,9 @@
+#include "0567.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.checkInclusion("ab", "eidbaooo") << '\n'
+         << sln.checkInclusion("ab", "eidboaoo") << '\n';
+    return 0;
+}
