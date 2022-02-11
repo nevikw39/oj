6c69066e590c2f11eb9be995010b3436708a473c
nevikw39 <nevik.w39@gmail.com>
2022-02-11 11:11:19 +0800

    Array Description

---
+++ CSES/1746_array_description.cpp
@@ -0,0 +1,61 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+constexpr int M = 1e9 + 7;
+
+int main()
+{
+    nevikw39;
+    int n, m, a, r = 0;
+    cin >> n >> m >> a;
+    vector<vector<int64_t>> dp(n, vector<int64_t>(m + 2));
+    if (a)
+        dp.front()[a] = 1;
+    else
+        fill(1 + ALL(dp.front()) - 1, 1);
+    for (int i = 1; i < n; i++)
+    {
+        cin >> a;
+        if (a)
+            dp[i][a] = (dp[i - 1][a - 1] + dp[i - 1][a] + dp[i - 1][a + 1]) % M;
+        else
+            for (int j = 1; j <= m; j++)
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j] + dp[i - 1][j + 1]) % M;
+    }
+    for (const auto &i : dp.back())
+        r = (r + i) % M;
+    cout << r << '\n';
+    return 0;
+}
