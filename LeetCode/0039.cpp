56b278412d8d542dc6064bf5506e05a6ee444235
nevikw39 <nevikw39@icloud.com>
2022-02-17 15:15:29 +0800

    Combination Sum

---
+++ LeetCode/0039.cpp
@@ -0,0 +1,7 @@
+#include "0039.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
---
+++ LeetCode/0039.hpp
@@ -0,0 +1,66 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+class Solution
+{
+private:
+    vector<vector<int>> y;
+    vector<int> v;
+    void f(const vector<int> &candidates, int target, int x = 0, int sum = 0)
+    {
+        if (x == candidates.size())
+        {
+            if (sum == target)
+                y.push_back(v);
+            return;
+        }
+        if (sum + candidates[x] <= target)
+        {
+            v.push_back(candidates[x]);
+            f(candidates, target, x, sum + candidates[x]);
+            v.pop_back();
+        }
+        f(candidates, target, x + 1, sum);
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
+        y.clear();
+        v.clear();
+        f(candidates, target);
+        return move(y);
+    }
+};
