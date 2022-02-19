88b56dad2550916bb51eb3781f8d30ac7b609de2
nevikw39 <nevikw39@icloud.com>
2022-02-19 22:34:25 +0800

    Count Equal and Divisible Pairs in an Array

---
+++ LeetCode/5996.cpp
@@ -0,0 +1,10 @@
+#include "5996.hpp"
+
+int main()
+{
+    vector<int> a = {3, 1, 2, 2, 2, 1, 3}, b = {1, 2, 3, 4};
+    Solution sln;
+    cout << sln.countPairs(a, 2) << '\n'
+         << sln.countPairs(b, 1) << '\n';
+    return 0;
+}
