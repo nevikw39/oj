de30ead835841b806e20c27971f220bb9e802d30
nevikw39 <nevikw39@icloud.com>
2022-02-19 22:38:05 +0800

    Find Three Consecutive Integers That Sum to a Given Number

---
+++ LeetCode/5997.cpp
@@ -0,0 +1,12 @@
+#include "5997.hpp"
+
+int main()
+{
+    Solution sln;
+    for (auto &&i : sln.sumOfThree(33))
+        cout << i << '\n';
+    cout << '\n';
+    for (auto &&i : sln.sumOfThree(4))
+        cout << i << '\n';
+    return 0;
+}
