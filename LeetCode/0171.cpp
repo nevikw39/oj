a74ee75fc6a2c57ebd5b3fa86146e9ed6e87ebc2
nevikw39 <nevikw39@icloud.com>
2022-02-22 16:19:17 +0800

    Excel Sheet Column Number

---
+++ LeetCode/0171.cpp
@@ -0,0 +1,10 @@
+#include "0171.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.titleToNumber("A") << '\n'
+         << sln.titleToNumber("AB") << '\n'
+         << sln.titleToNumber("ZY") << '\n';
+    return 0;
+}
