78232ec913982a69879c0c93d82d492cb6330de1
nevikw39 <nevikw39@icloud.com>
2022-02-24 16:42:21 +0800

    Sort List

---
+++ LeetCode/0148.cpp
@@ -0,0 +1,16 @@
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#include "0148.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
