8673b23c8b07814205c04e8d04030e63a92c98ae
nevikw39 <nevik.w39@gmail.com>
2022-02-26 20:54:25 +0800

    AtCoder Beginner Contest 241 p. C

---
+++ AtCoder/Beginner_241/c.cpp
@@ -0,0 +1,106 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <atcoder/all>
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int n;
+    cin >> n;
+    vector<string> s(n), d(n << 1), dd(n << 1);
+    for (auto &i : s)
+        cin >> i;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j + 6 <= n; j++)
+        {
+            int x = 0;
+            for (int k = 0; k < 6; k++)
+                if (s[i][j + k] == '.')
+                    ++x;
+            if (x <= 2)
+            {
+                cout << "Yes\n";
+                return 0;
+            }
+        }
+    for (int i = 0; i + 6 <= n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            int x = 0;
+            for (int k = 0; k < 6; k++)
+                if (s[i + k][j] == '.')
+                    ++x;
+            if (x <= 2)
+            {
+                cout << "Yes\n";
+                return 0;
+            }
+        }
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            d[i + j].push_back(s[i][j]);
+            dd[i - j + n - 1].push_back(s[i][j]);
+        }
+    for (const auto &i : d)
+        for (int j = 0, l = i.length(); j + 6 <= l; j++)
+        {
+            int x = 0;
+            for (int k = 0; k < 6; k++)
+                if (i[j + k] == '.')
+                    ++x;
+            if (x <= 2)
+            {
+                cout << "Yes\n";
+                return 0;
+            }
+        }
+    for (const auto &i : dd)
+        for (int j = 0, l = i.length(); j + 6 <= l; j++)
+        {
+            int x = 0;
+            for (int k = 0; k < 6; k++)
+                if (i[j + k] == '.')
+                    ++x;
+            if (x <= 2)
+            {
+                cout << "Yes\n";
+                return 0;
+            }
+        }
+    cout << "No\n";
+    return 0;
+}
