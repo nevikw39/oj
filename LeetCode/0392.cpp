55498c6180c5db2756b92675a0e460a881b486c7
nevikw39 <nevikw39@icloud.com>
2022-03-02 17:20:38 +0800

    Is Subsequence

---
+++ LeetCode/0392.cpp
@@ -0,0 +1,9 @@
+#include "0392.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.isSubsequence("abc", "ahbgdc") << '\n'
+         << sln.isSubsequence("axc", "ahbgdc") << '\n';
+    return 0;
+}
