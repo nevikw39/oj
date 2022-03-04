564566f4eea073616ff09155d921a78cb9f1911d
nevikw39 <nevikw39@icloud.com>
2022-03-04 14:01:37 +0800

    Champagne Tower

---
+++ LeetCode/0799.cpp
@@ -0,0 +1,10 @@
+#include "0799.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.champagneTower(1, 1, 1) << '\n'
+         << sln.champagneTower(2, 1, 1) << '\n'
+         << sln.champagneTower(100000009, 33, 17) << '\n';
+    return 0;
+}
