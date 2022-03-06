fb565d4f2f9de4bf47a279dfddfd0b282e5a7392
nevikw39 <nevikw39@icloud.com>
2022-03-06 14:34:23 +0800

    Count All Valid Pickup and Delivery Options

---
+++ LeetCode/1359.cpp
@@ -0,0 +1,10 @@
+#include "1359.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.countOrders(1) << '\n'
+         << sln.countOrders(2) << '\n'
+         << sln.countOrders(3) << '\n';
+    return 0;
+}
