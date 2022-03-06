685149709add486f1f2e16d268f0d3dba668b707
nevikw39 <nevikw39@icloud.com>
2022-03-06 11:39:00 +0800

    Replace Non-Coprime Numbers in Array

---
+++ LeetCode/6019.cpp
@@ -0,0 +1,16 @@
+#include "6019.hpp"
+
+int main()
+{
+    vector<int> a = {6, 4, 3, 2, 7, 6, 2}, b = {2, 2, 1, 1, 3, 3, 3}, c = {287, 41, 49, 287, 899, 23, 23, 20677, 5, 825};
+    Solution sln;
+    for (auto &&i : sln.replaceNonCoprimes(a))
+        cout << i << '\n';
+    cout << '\n';
+    for (auto &&i : sln.replaceNonCoprimes(b))
+        cout << i << '\n';
+    cout << '\n';
+    for (auto &&i : sln.replaceNonCoprimes(c))
+        cout << i << '\n';
+    return 0;
+}
