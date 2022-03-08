5080202934d34b2db7be82af1e878dae567e0b2d
nevikw39 <nevikw39@icloud.com>
2022-03-08 17:01:54 +0800

    Linked List Cycle

---
+++ LeetCode/0141.cpp
@@ -0,0 +1,14 @@
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+#include "0141.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
