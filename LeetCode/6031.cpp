1d72fb2b5d5dec9c39e9d9ce976a238ef88bad03
nevikw39 <nevik.w39@gmail.com>
2022-03-13 11:02:08 +0800

    Find All K-Distant Indices in an Array

---
+++ LeetCode/6031.cpp
@@ -0,0 +1,13 @@
+#include "6031.hpp"
+
+int main()
+{
+    vector<int> a = {3, 4, 9, 1, 3, 9, 5}, b = {2, 2, 2, 2, 2};
+    Solution sln;
+    for (auto &&i : sln.findKDistantIndices(a, 9, 1))
+        cout << i << '\n';
+    cout << '\n';
+    for (auto &&i : sln.findKDistantIndices(b, 2, 2))
+        cout << i << '\n';
+    return 0;
+}
