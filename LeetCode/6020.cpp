681ab4634ec9df946723362072ec0a79db797c41
nevikw39 <nevikw39@icloud.com>
2022-03-19 22:33:58 +0800

    Divide Array Into Equal Pairs

---
+++ LeetCode/6020.cpp
@@ -0,0 +1,10 @@
+#include "6020.hpp"
+
+int main()
+{
+    vector<int> a = {3, 2, 3, 2, 2, 2}, b = {1, 2, 3, 4};
+    Solution sln;
+    cout << sln.divideArray(a) << '\n'
+         << sln.divideArray(b) << '\n';
+    return 0;
+}
