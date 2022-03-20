baddb2bee830ec71fd19e714e0832583ae04d4a6
nevikw39 <nevikw39@icloud.com>
2022-03-20 10:38:23 +0800

    Count Hills and Valleys in an Array

---
+++ LeetCode/6027.cpp
@@ -0,0 +1,10 @@
+#include "6027.hpp"
+
+int main()
+{
+    vector<int> a = {2, 4, 1, 1, 6, 5}, b = {6, 6, 5, 5, 4, 1};
+    Solution sln;
+    cout << sln.countHillValley(a) << '\n'
+         << sln.countHillValley(b) << '\n';
+    return 0;
+}
