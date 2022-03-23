7a489015f64028bc7b6e69d9386adc87d21a3b32
nevikw39 <nevikw39@icloud.com>
2022-03-23 15:49:03 +0800

    Broken Calculator

---
+++ LeetCode/0991.cpp
@@ -0,0 +1,10 @@
+#include "0991.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.brokenCalc(2, 3) << '\n'
+         << sln.brokenCalc(5, 8) << '\n'
+         << sln.brokenCalc(3, 10) << '\n';
+    return 0;
+}
