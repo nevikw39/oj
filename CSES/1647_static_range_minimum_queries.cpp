67e006bb9f1c6ea4728672d1fb78bafa932a698a
nevikw39 <nevikw39@icloud.com>
2022-03-27 16:40:07 +0800

    Static Range Minimum Queries

---
+++ CSES/1647_static_range_minimum_queries.cpp
@@ -0,0 +1,57 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int n, q;
+    cin >> n >> q;
+    vector<vector<int>> st(n + 1, vector<int>(__lg(n) + 1));
+    for (int i = 0; i < n; i++)
+        cin >> st[i][0];
+    for (int j = 1; j <= __lg(n); j++)
+        for (int i = 0; i + (1 << j) <= n; i++)
+            st[i][j] = min(st[i][j - 1], st[i + (1 << j - 1)][j - 1]);
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+        --a;
+        --b;
+        int x = __lg(b - a + 1);
+        cout << min(st[a][x], st[b - (1 << x) + 1][x]) << '\n';
+    }
+    return 0;
+}
