ba662486cd4cb9418fcfaa82b7df1227cc4f75c8
nevikw39 <nevikw39@icloud.com>
2022-03-27 16:30:46 +0800

    Company Queries I

---
+++ CSES/1687_company_queries_i.cpp
@@ -0,0 +1,60 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int n, q;
+    cin >> n >> q;
+    vector<vector<int>> a(n + 1, vector<int>(__lg(n) + 1));
+    for (int i = 2; i <= n; i++)
+        cin >> a[i][0];
+    for (int j = 1; j <= __lg(n); j++)
+        for (int i = 1; i <= n; i++)
+            a[i][j] = a[a[i][j - 1]][j - 1];
+    while (q--)
+    {
+        int e, k;
+        cin >> e >> k;
+        for (int i = 0; i <= __lg(n); i++, k >>= 1)
+            if (k & 1)
+                e = a[e][i];
+        if (e)
+            cout << e << '\n';
+        else
+            cout << "-1\n";
+    }
+    return 0;
+}
