38a911fbc763509533800ab0d36c2eefc9ad83c6
nevikw39 <nevikw39@icloud.com>
2022-03-27 10:49:58 +0800

    Minimum Deletions to Make Array Beautiful

---
+++ LeetCode/5236.cpp
@@ -0,0 +1,11 @@
+#include "5236.hpp"
+
+int main()
+{
+    vector<int> a = {1, 1, 2, 3, 5}, b = {1, 1, 2, 2, 3, 3}, c = {5, 1, 5, 4, 8, 1, 4, 4, 1, 9, 2, 2, 2, 5, 1};
+    Solution sln;
+    cout << sln.minDeletion(a) << '\n'
+         << sln.minDeletion(b) << '\n'
+         << sln.minDeletion(c) << '\n';
+    return 0;
+}
