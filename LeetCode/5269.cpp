8def3715f0a1f7aa15a7c049e60cbd019fde65fd
nevikw39 <nevikw39@icloud.com>
2022-03-27 11:39:52 +0800

    Maximum Value of K Coins From Piles

---
+++ LeetCode/5269.cpp
@@ -0,0 +1,10 @@
+#include "5269.hpp"
+
+int main()
+{
+    vector<vector<int>> a = {{1, 100, 3}, {7, 8, 9}}, b = {{100}, {100}, {100}, {100}, {100}, {100}, {1, 1, 1, 1, 1, 1, 700}};
+    Solution sln;
+    cout << sln.maxValueOfCoins(a, 2) << '\n'
+         << sln.maxValueOfCoins(b, 7) << '\n';
+    return 0;
+}
