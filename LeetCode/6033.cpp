2136412c03e0f9e25c15d863726a2eb08e1d267d
nevikw39 <nevik.w39@gmail.com>
2022-04-02 22:38:13 +0800

    Minimum Bit Flips to Convert Number

---
+++ LeetCode/6033.cpp
@@ -0,0 +1,10 @@
+#include "6033.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.minBitFlips(10, 7) << '\n'
+         << sln.minBitFlips(3, 4) << '\n'
+         << sln.minBitFlips(10, 82) << '\n';
+    return 0;
+}
