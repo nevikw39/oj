dd7ff9c33b83805a79aa93c010436e2399f82918
nevikw39 <nevikw39@icloud.com>
2022-04-07 15:04:18 +0800

    Last Stone Weight

---
+++ LeetCode/1046.cpp
@@ -0,0 +1,10 @@
+#include "1046.hpp"
+
+int main()
+{
+    vector<int> a = {2, 7, 4, 1, 8, 1}, b = {1};
+    Solution sln;
+    cout << sln.lastStoneWeight(a) << '\n'
+         << sln.lastStoneWeight(b) << '\n';
+    return 0;
+}
