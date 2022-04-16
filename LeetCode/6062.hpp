5b8636d9b7c1c336382dc2ea5ed38c03f9c9e13c
nevikw39 <nevikw39@icloud.com>
2022-04-16 22:59:02 +0800

    Design an ATM Machine

---
+++ LeetCode/6062.hpp
@@ -0,0 +1,64 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+class ATM
+{
+private:
+    array<int64_t, 5> arr, brr = {20, 50, 100, 200, 500};
+
+public:
+    ATM() { fill(ALL(arr), 0); }
+
+    void deposit(const vector<int> &banknotesCount)
+    {
+        for (int i = 0; i < 5; i++)
+            arr[i] += banknotesCount[i];
+    }
+
+    vector<int> withdraw(int amount)
+    {
+        auto tmp = arr;
+        vector<int> y(5);
+        for (int i = 4; i >= 0; i--)
+        {
+            arr[i] -= (y[i] = min(amount / brr[i], arr[i]));
+            amount -= y[i] * brr[i];
+        }
+        if (!amount)
+            return move(y);
+        arr = tmp;
+        return {-1};
+    }
+};
