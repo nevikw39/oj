265f43f43d6f460121d7c1a493b4d44dfa777aad
nevikw39 <nevikw39@icloud.com>
2022-04-16 22:41:53 +0800

    Number of Ways to Buy Pens and Pencils

---
+++ LeetCode/6061.cpp
@@ -0,0 +1,10 @@
+#include "6061.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.waysToBuyPensPencils(20, 10, 5) << '\n'
+         << sln.waysToBuyPensPencils(5, 10, 10) << '\n'
+         << sln.waysToBuyPensPencils(10, 2, 5) << '\n';
+    return 0;
+}
