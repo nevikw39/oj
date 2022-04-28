f39b9fcb278045f4732a4685dc69f0dad1e0aae6
nevikw39 <nevikw39@icloud.com>
2022-04-28 16:05:24 +0800

    Path With Minimum Effort

---
+++ LeetCode/1631.cpp
@@ -0,0 +1,9 @@
+#include "1631.hpp"
+
+int main()
+{
+    vector<vector<int>> a = {{1, 2, 2}, {3, 8, 2}, {5, 3, 5}};
+    Solution sln;
+    cout << sln.minimumEffortPath(a) << '\n';
+    return 0;
+}
