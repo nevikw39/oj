21658af4e7a802a43f3dfb2bac7db637c815b825
nevikw39 <nevikw39@icloud.com>
2022-05-08 23:08:39 +0800

    Flatten Nested List Iterator

---
+++ LeetCode/0341.hpp
@@ -0,0 +1,68 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+class NestedIterator
+{
+private:
+    deque<NestedInteger> dq;
+
+public:
+    NestedIterator(vector<NestedInteger> &nestedList)
+    {
+        for (const auto &i : nestedList)
+            dq.push_front(i);
+    }
+
+    int next()
+    {
+        int y = dq.back().getInteger();
+        dq.pop_back();
+        return y;
+    }
+
+    bool hasNext()
+    {
+        while (dq.size())
+        {
+            if (dq.back().isInteger())
+                return true;
+            auto a = dq.back().getList();
+            dq.pop_back();
+            for (auto itr = a.crbegin(); itr != a.crend(); itr++)
+                dq.push_back(*itr);
+        }
+        return false;
+    }
+};
