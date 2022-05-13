20434e9a8672e84a9a94747de4de01dbfd182919
nevikw39 <nevik.w39@gmail.com>
2022-05-13 13:25:51 +0800

    Populating Next Right Pointers in Each Node II

---
+++ LeetCode/0117.cpp
@@ -0,0 +1,23 @@
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node *next;
+
+    Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
+
+    Node(int _val) : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
+
+    Node(int _val, Node *_left, Node *_right, Node *_next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+#include "0117.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
