5ca66a79ca712a64fcba7342076f03d8398e2ff1
nevikw39 <nevik.w39@gmail.com>
2022-05-14 20:26:27 +0800

    AtCoder Beginner Contest 251 C - Poem Online Judge

---
+++ AtCoder/Beginner_251/c.cpp
@@ -0,0 +1,56 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <atcoder/all>
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int n;
+    cin >> n;
+    _hash<string, pair<int, int>> ht;
+    for (int i = 1; i <= n; i++)
+    {
+        string s;
+        int t;
+        cin >> s >> t;
+        if (ht.find(s) != ht.end())
+            continue;
+        ht[s] = {t, i};
+    }
+    pair<int, int> r{INT_MIN, n};
+    for (const auto &[_, i] : ht)
+        if (r.ST < i.ST || r.ST == i.ST && r.ND > i.ND)
+            r = i;
+    cout << r.ND << '\n';
+    return 0;
+}
