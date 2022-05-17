440a3457e632b8b1d432b81f3a6d3652cf7ec7e6
nevikw39 <nevik.w39@gmail.com>
2022-05-18 00:23:31 +0800

    Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ LeetCode/1379.cpp
@@ -0,0 +1,15 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+#include "1379.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
