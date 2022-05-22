39b8260f587d65950d6d31ea8ea821861c01b535
nevikw39 <nevik.w39@gmail.com>
2022-05-22 22:05:09 +0800

    Palindromic Deletions `(Partial)`

---
+++ KickStart/2022_C/d.cpp
@@ -0,0 +1,78 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+constexpr int M = 1e9 + 7;
+
+int64_t binpow(int64_t a, int n, int m = M)
+{
+    int64_t y = 1;
+    for (a %= m; n; n >>= 1, (a *= a) %= m)
+        if (n & 1)
+            (y *= a) %= m;
+    return y;
+}
+
+int64_t inline inv(int x, int m = M) { return binpow(x, m - 2, m); }
+
+int64_t f(string s)
+{
+    int64_t l = s.length(), y = 0;
+    if (l == 1)
+        return 1;
+    for (int i = 0; i < l; i++)
+    {
+        string t = s.substr(0, i) + s.substr(i + 1);
+        y += (t == string(t.rbegin(), t.rend())) + f(t);
+    }
+    return (y % M * inv(l) % M + M) % M;
+}
+
+auto solve()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+    return f(s);
+}
+
+int main()
+{
+    nevikw39;
+    int i = 0, t;
+    cin >> t;
+    while (i < t)
+        cout << "Case #" << ++i << ": " << solve() << '\n';
+    return 0;
+}
