a06e16d5202e6d52a21b0097c5505f33d24a68be
nevikw39 <nevik.w39@gmail.com>
2022-05-27 10:37:37 +0800

    Number of Steps to Reduce a Number to Zero

---
+++ LeetCode/1342.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "1342.h"
+
+int main()
+{
+    printf("%d\n%d\n%d\n", numberOfSteps(14), numberOfSteps(8), numberOfSteps(123));
+    return 0;
+}
