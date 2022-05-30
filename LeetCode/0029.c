722b22a94955f6e91e048292ef0b72558bf30665
nevikw39 <nevik.w39@gmail.com>
2022-05-30 15:12:46 +0800

    Divide Two Integers

---
+++ LeetCode/0029.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "0029.h"
+
+int main()
+{
+    printf("%d\n%d\n", divide(10, 3), divide(7, -3));
+    return 0;
+}
