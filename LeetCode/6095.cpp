99663dc5f0480653ad4b2c94a0d76d817609e6fc
nevikw39 <nevikw39@icloud.com>
2022-06-11 22:38:29 +0800

    Strong Password Checker II

---
+++ LeetCode/6095.cpp
@@ -0,0 +1,10 @@
+#include "6095.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.strongPasswordCheckerII("IloveLe3tcode!") << '\n'
+         << sln.strongPasswordCheckerII("Me+You--IsMyDream") << '\n'
+         << sln.strongPasswordCheckerII("1aB!") << '\n';
+    return 0;
+}
