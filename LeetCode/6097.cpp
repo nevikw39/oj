e382875fb49f04b66d8e4652a62fb6d5dadead9f
nevikw39 <nevikw39@icloud.com>
2022-06-11 23:43:54 +0800

    Match Substring After Replacement

---
+++ LeetCode/6097.cpp
@@ -0,0 +1,11 @@
+#include "6097.hpp"
+
+int main()
+{
+    vector<vector<char>> a = {{'e', '3'}, {'t', '7'}, {'t', '8'}}, b = {{'o', '0'}}, c = {{'e', '3'}, {'t', '7'}, {'t', '8'}, {'d', 'b'}, {'p', 'b'}};
+    Solution sln;
+    cout << sln.matchReplacement("fool3e7bar", "leet", a) << '\n'
+         << sln.matchReplacement("fooleetbar", "f00l", b) << '\n'
+         << sln.matchReplacement("Fool33tbaR", "leetd", c) << '\n' << '\n';
+    return 0;
+}
