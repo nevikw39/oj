257c5b86bd47b3f355fbb6af74fcfccea38abe6f
nevikw39 <nevikw39@icloud.com>
2022-06-13 01:59:54 +0800

    Maximum Erasure Value

---
+++ LeetCode/1695.cpp
@@ -0,0 +1,7 @@
+#include "1695.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
---
+++ LeetCode/1695.hpp
@@ -0,0 +1,72 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+struct _hash_fn
+{
+    static inline uint64_t splitmix64(uint64_t x)
+    {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+class Solution
+{
+public:
+    int maximumUniqueSubarray(vector<int> &nums)
+    {
+        int y = 0;
+        _hash<int, null_type, _hash_fn> hs;
+        for (int s = 0, l = 0, r = 0, n = nums.size(); r < n; r++)
+        {
+            s += nums[r];
+            while (hs.find(nums[r]) != hs.end())
+            {
+                hs.erase(nums[l]);
+                s -= nums[l++];
+            }
+            hs.insert(nums[r]);
+            y = max(y, s);
+        }
+        return y;
+    }
+};
