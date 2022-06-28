c76b2715fc93bb24a0924f0e0fb0e9477a00bf63
nevikw39 <nevik.w39@gmail.com>
2022-06-28 16:16:41 +0800

    Minimum Deletions to Make Character Frequencies

---
+++ LeetCode/1647.cpp
@@ -0,0 +1,10 @@
+#include "1647.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.minDeletions("aab") << '\n'
+         << sln.minDeletions("aaabbbcc") << '\n'
+         << sln.minDeletions("ceabaacb") << '\n';
+    return 0;
+}
