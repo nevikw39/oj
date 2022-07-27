e8239f1ba42ff0a555548591de68fa6a42c3908b
nevikw39 <nevik.w39@gmail.com>
2022-07-28 01:32:32 +0800

    Flatten Binary Tree to Linked List

---
+++ LeetCode/0114.cpp
@@ -0,0 +1,14 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+#include "0114.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
