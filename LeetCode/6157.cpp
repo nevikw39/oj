fbfbd7488964192bb031c7d810254df014007142
nevikw39 <nevik.w39@gmail.com>
2022-08-20 22:55:59 +0800

    Time Needed to Rearrange a Binary String

---
+++ LeetCode/6157.cpp
@@ -0,0 +1,9 @@
+#include "6157.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.secondsToRemoveOccurrences("0110101") << '\n'
+         << sln.secondsToRemoveOccurrences("11100") << '\n';
+    return 0;
+}
