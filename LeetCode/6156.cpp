08b3057677771099b852d7a22b2288df46d67675
nevikw39 <nevik.w39@gmail.com>
2022-08-20 22:34:53 +0800

    Minimum Recolors to Get K Consecutive Black Blocks

---
+++ LeetCode/6156.cpp
@@ -0,0 +1,9 @@
+#include "6156.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.minimumRecolors("WBBWWBBWBW", 7) << '\n'
+         << sln.minimumRecolors("WBWBBBW", 2) << '\n';
+    return 0;
+}
