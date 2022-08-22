98973812a04b2644f2c44ff6362d39ff8d7ae9d7
nevikw39 <nevik.w39@gmail.com>
2022-08-22 10:36:00 +0800

    Power of Four

---
+++ LeetCode/0342.c
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "0342.h"
+
+int main()
+{
+    printf("%d\n%d\n%d\n", isPowerOfFour(16), isPowerOfFour(5), isPowerOfFour(1));
+    return 0;
+}
