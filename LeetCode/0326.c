fa5ecd2c04aa4bc7a4ec358019dc61ded31bee79
nevikw39 <nevik.w39@gmail.com>
2022-08-24 14:22:07 +0800

    Power of Three

---
+++ LeetCode/0326.c
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "0326.h"
+
+int main()
+{
+    printf("%d\n%d\n%d\n", isPowerOfThree(27), isPowerOfThree(0), isPowerOfThree(9));
+    return 0;
+}
---
+++ LeetCode/0326.h
@@ -0,0 +1,17 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+unsigned int arr[] = {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907, 43046721, 129140163, 387420489, 1162261467};
+
+int f(int *lhs, unsigned int *rhs) { return *lhs - *rhs; }
+
+inline bool isPowerOfThree(int n) { return bsearch(&n, arr, 20, sizeof(int), f); }
