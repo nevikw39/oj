0438a2d76f4d9433c0b81cc924ae4efb8fa7e217
nevikw39 <nevik.w39@gmail.com>
2022-08-29 14:15:10 +0800

    Google Kick Start 2022 Practice Session #3 - GBus count

---
+++ KickStart/2022_practice_3/d.cpp
@@ -0,0 +1,69 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+constexpr int N = 5e3 + 5;
+
+auto solve()
+{
+    stringstream ss;
+    int n, p, arr[N] = {0};
+    cin >> n;
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+        ++arr[a];
+        --arr[b + 1];
+    }
+    partial_sum(arr, arr + N, arr);
+    cin >> p;
+    while (p--)
+    {
+        int c;
+        cin >> c;
+        ss << arr[c] << ' ';
+    }
+    return ss.str();
+}
+
+int main()
+{
+    nevikw39;
+    int i = 0, t;
+    cin >> t;
+    while (i < t)
+        cout << "Case #" << ++i << ": " << solve() << '\n';
+    return 0;
+}
