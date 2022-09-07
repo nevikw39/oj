2dfb7cb20681b6186e02623886d90fe9f65c6e5f
nevikw39 <nevikw39@icloud.com>
2022-09-08 02:55:31 +0800

    Construct String from Binary Tree

---
+++ LeetCode/0606.cpp
@@ -0,0 +1,16 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left, *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#include "0606.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
