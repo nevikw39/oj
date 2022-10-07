233c70299d5ee12524ef66126d5314de6d637b00
nevikw39 <nevik.w39@gmail.com>
2022-10-07 17:30:18 +0800

    Square

---
+++ UVa/uva10364.cpp
@@ -0,0 +1,81 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int m, sum;
+vector<int> sticks;
+vector<bool> v;
+
+bool f(int i = 0, int j = 0, int len = 0)
+{
+    if (len == sum)
+    {
+        len = i = 0;
+        if (++j == 4)
+            return true;
+    }
+    if (i == m)
+        return false;
+    if (f(i + 1, j, len))
+        return true;
+    if (!v[i] && len + sticks[i] <= sum)
+    {
+        v[i] = true;
+        if (f(i + 1, j, len + sticks[i]))
+            return true;
+        v[i] = false;
+    }
+    return false;
+}
+
+void solve()
+{
+    cin >> m;
+    sticks.assign(m, 0);
+    v.assign(m, false);
+    for (int &i : sticks)
+        cin >> i;
+    sum = accumulate(ALL(sticks), 0);
+    if (sum & 3)
+    {
+        cout << "no\n";
+        return;
+    }
+    sum >>= 2;
+    sort(ALL(sticks), greater<>());
+    cout << (f() ? "yes\n" : "no\n");
+}
+
+int main()
+{
+    nevikw39;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
