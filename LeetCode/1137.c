6cc1de2f8f6a8b0bb234f0ac477e08a76b8493cf
nevikw39 <nevik.w39@gmail.com>
2023-01-30 12:11:12 +0800

    N-th Tribonacci Number

---
+++ LeetCode/1137.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "1137.h"
+
+int main()
+{
+    printf("%d\n%d\n", tribonacci(4), tribonacci(25));
+    return 0;
+}
