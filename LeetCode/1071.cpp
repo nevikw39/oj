0795c3f18672e40b118f410fc681760aa5eaf714
nevikw39 <nevik.w39@gmail.com>
2023-02-01 13:41:09 +0800

    Greatest Common Divisor of Strings

---
+++ LeetCode/1071.cpp
@@ -0,0 +1,10 @@
+#include "1071.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.gcdOfStrings("ABCABC", "ABC") << '\n'
+         << sln.gcdOfStrings("ABABAB", "ABAB") << '\n'
+         << sln.gcdOfStrings("LEET", "CODE") << '\n';
+    return 0;
+}
