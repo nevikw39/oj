c4675acd94f5cf41a91ffc598a5dc4cf34b60c1a
nevikw39 <nevikw39@icloud.com>
2023-02-12 13:28:13 +0800

    Minimum Fuel Cost to Report to the Capital

---
+++ LeetCode/2477.hpp
@@ -0,0 +1,71 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+class Solution
+{
+public:
+    long long minimumFuelCost(vector<vector<int>> &roads, int seats)
+    {
+        int n = roads.size() + 1;
+        vector<vector<int>> g(n);
+        vector<int> deg(n), sz(n, 1);
+        for (const auto &i : roads)
+        {
+            g[i[0]].push_back(i[1]);
+            g[i[1]].push_back(i[0]);
+            ++deg[i[0]];
+            ++deg[i[1]];
+        }
+        int64_t y = 0;
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+            if (deg[i] == 1)
+                q.push(i);
+        while (q.size())
+        {
+            int f = q.front();
+            q.pop();
+            y += (sz[f] + seats - 1) / seats; // ceil(sz[f] / seats)
+            for (int i : g[f])
+            {
+                --deg[i];
+                sz[i] += sz[f];
+                if (i && deg[i] == 1)
+                    q.push(i);
+            }
+        }
+        return y;
+    }
+};
