9aaf9549e7f298fb65bc0f16cd1c0a1e8d78cd8f
nevikw39 <nevikw39@icloud.com>
2023-02-13 16:10:21 +0800

    Count Odd Numbers in an Interval Range

---
+++ LeetCode/1523.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "1523.h"
+
+int main()
+{
+    printf("%d\n%d\n", countOdds(3, 7), countOdds(8, 10));
+    return 0;
+}
