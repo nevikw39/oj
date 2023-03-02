706256c0dde164ff64876e39c5e64a8ef251209e
nevikw39 <nevikw39@icloud.com>
2023-03-02 22:45:10 +0800

    Codeforces Round #855 (Div. 3) B - Count the Number of Pairs

---
+++ Codeforces/1800/b.cpp
@@ -0,0 +1,62 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int n, k, low[26] = {0}, up[26] = {0}, a = 0, b = 0;
+    string s;
+    cin >> n >> k >> s;
+    for (char c : s)
+        if (c & ' ')
+            ++low[c - 'a'];
+        else
+            ++up[c - 'A'];
+    for (int i = 0; i < 26; i++)
+    {
+        a += min(low[i], up[i]);
+        b += abs(low[i] - up[i]) >> 1;
+    }
+    cout << a + min(b, k) << '\n';
+}
+
+int main()
+{
+    nevikw39;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
