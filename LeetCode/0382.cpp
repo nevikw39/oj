9df869b88cfc01c9a487dd6cd83d551373c569e4
nevikw39 <nevikw39@icloud.com>
2023-03-10 12:22:33 +0800

    Linked List Random Node

---
+++ LeetCode/0382.cpp
@@ -0,0 +1,16 @@
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#include "0382.hpp"
+
+int main()
+{
+    Solution sln(nullptr);
+    return 0;
+}
---
+++ LeetCode/0382.hpp
@@ -0,0 +1,59 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+class Solution
+{
+private:
+    vector<int> v;
+    static random_device rd;
+    static mt19937 mt;
+    uniform_int_distribution<> uid;
+
+public:
+    Solution(ListNode *head)
+    {
+        while (head)
+        {
+            v.push_back(head->val);
+            head = head->next;
+        }
+        uid.param(decltype(uid)::param_type(0, v.size() - 1));
+    }
+
+    int getRandom() { return v[uid(mt)]; }
+};
+
+random_device Solution::rd;
+mt19937 Solution::mt(Solution::rd());
