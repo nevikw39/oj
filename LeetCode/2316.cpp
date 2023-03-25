4b5e89c26dfd709af25fa167722cfe9ba0c8dd34
nevikw39 <nevikw39@icloud.com>
2023-03-25 17:50:44 +0800

    Count Unreachable Pairs of Nodes in an Undirected Graph

---
+++ LeetCode/2316.cpp
@@ -0,0 +1,7 @@
+#include "2316.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
---
+++ LeetCode/2316.hpp
@@ -0,0 +1,78 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+class Solution
+{
+private:
+    struct disjoint_set
+    {
+        int n, g;
+        vector<int> p, sz;
+
+        disjoint_set() {}
+        disjoint_set(int _n) : n(_n), g(_n), p(_n), sz(_n, 1) { iota(ALL(p), 0); }
+
+        int find(int x) { return p[x] == x ? x : p[x] = find(p[x]); }
+
+        int unite(int x, int y)
+        {
+            x = find(x);
+            y = find(y);
+            if (x != y)
+            {
+                --g;
+                if (sz[x] < sz[y])
+                    swap(x, y);
+                sz[p[y] = x] += sz[y];
+            }
+            return x;
+        }
+
+        bool same(int x, int y) { return find(x) == find(y); }
+    } dsu;
+
+public:
+    long long countPairs(int n, vector<vector<int>> &edges)
+    {
+        dsu = disjoint_set(n);
+        for (const auto &i : edges)
+            dsu.unite(i[0], i[1]);
+        int64_t y = 0;
+        for (int i = 0; i < n; i++)
+            if (dsu.find(i) == i)
+                y += 1LL * dsu.sz[i] * (n - dsu.sz[i]);
+        return y >> 1;
+    }
+};
