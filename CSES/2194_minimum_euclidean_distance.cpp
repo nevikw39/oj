9a0397c33803e871b145ef412f2f80ffec9668c8
nevikw39 <nevik.w39@gmail.com>
2023-04-02 17:58:43 +0800

    Minimum Euclidean Distance

---
+++ CSES/2194_minimum_euclidean_distance.cpp
@@ -0,0 +1,94 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr), ios::sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = less<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = cc_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+template <typename T = int>
+T sign(T x) { return (x > 0) - (x < 0); }
+
+template <typename T = int>
+struct point
+{
+    T x, y;
+    point operator+(const point &p) const { return point{x + p.x, y + p.y}; }
+    point operator-(const point &p) const { return point{x - p.x, y - p.y}; }
+    bool operator==(const point &p) const { return x == p.x && y == p.y; }
+    T dot(const point &p) const { return x * p.x + y * p.y; }
+    T cross(const point &p) const { return x * p.y - y * p.x; }
+    point normal() const { return point{-y, x}; }
+    T abs2() const { return dot(*this); }
+    long double rad(const point &p) const { return abs(atan2(abs(cross(p)), dot(p))); }
+};
+
+template <typename T = int64_t, typename Itr = typename vector<point<T>>::iterator>
+T closest_pair(Itr begin, Itr end)
+{
+    if (end - begin <= 1)
+        return numeric_limits<T>::max();
+    Itr mid = begin + (end - begin >> 1);
+    T x = mid->x, y = min(closest_pair(begin, mid), closest_pair(mid, end));
+    inplace_merge(begin, mid, end, [](const auto &lhs, const auto &rhs)
+                  { return lhs.y < rhs.y; });
+    deque<point<T>> dq;
+    for (Itr itr = begin; itr != end; itr++)
+        if (y > point<T>{itr->x - x}.abs2())
+        {
+            for (auto j : dq)
+            {
+                if (y <= point<T>{j.y - itr->y}.abs2())
+                    break;
+                y = min(y, (j - *itr).abs2());
+            }
+            dq.emplace_front(*itr);
+        }
+    return y;
+}
+template <typename T = int64_t>
+T closest_pair(vector<point<T>> points)
+{
+    sort(ALL(points), [](const auto &lhs, const auto &rhs)
+         { return lhs.x < rhs.x; });
+    return closest_pair(ALL(points));
+}
+
+int main()
+{
+    nevikw39;
+    int n;
+    cin >> n;
+    vector<point<int64_t>> points(n);
+    for (auto &[x, y] : points)
+        cin >> x >> y;
+    cout << closest_pair(points) << '\n';
+    return 0;
+}
