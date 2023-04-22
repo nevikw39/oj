20216c3800b1eb6e55a7bcf74aaa75a8c0aed684
nevikw39 <nevik.w39@gmail.com>
2023-04-22 21:47:52 +0800

    Minimum Insertion Steps to Make a String Palindrome

---
+++ LeetCode/1312.cpp
@@ -0,0 +1,10 @@
+#include "1312.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.minInsertions("zzazz") << '\n'
+         << sln.minInsertions("mbadm") << '\n'
+         << sln.minInsertions("leetcode") << '\n';
+    return 0;
+}
