427dab3dca47b90b3594ee2844ebbf4f3529853f
nevikw39 <nevikw39@icloud.com>
2023-04-23 14:38:21 +0800

    Restore The Array

---
+++ LeetCode/1416.cpp
@@ -0,0 +1,10 @@
+#include "1416.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.numberOfArrays("1000", 10000) << '\n'
+         << sln.numberOfArrays("1000", 10) << '\n'
+         << sln.numberOfArrays("1317", 2000) << '\n';
+    return 0;
+}
