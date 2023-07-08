0610afcb8f9708925ae22053ad75564963595670
nevikw39 <nevik.w39@gmail.com>
2023-07-08 20:24:31 +0800

    AtCoder Beginner Contest 309 B - Rotate

---
+++ AtCoder/Beginner_309/b.cpp
@@ -0,0 +1,58 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <atcoder/all>
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int n;
+    cin >> n;
+    vector<string> a(n);
+    for (auto &i : a)
+        cin >> i;
+    auto b = a;
+    rotate(b.front().rbegin(), b.front().rbegin() + 1, b.front().rend());
+    rotate(b.back().begin(), 1 + ALL(b.back()));
+    b.front().front() = a[1].front();
+    b.back().back() = a[n - 2].back();
+    for (int i = 1; i + 1 < n; i++)
+    {
+        b[i].front() = a[i + 1].front();
+        b[i].back() = a[i - 1].back();
+    }
+    copy(ALL(b), ostream_iterator<string>(cout, "\n"));
+    return 0;
+}
