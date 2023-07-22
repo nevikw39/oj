adb6ac2d14201830b820814b01adda6b0e43fb16
nevikw39 <nevik.w39@gmail.com>
2023-07-22 20:19:41 +0800

    AtCoder Beginner Contest 311 C - Find it!

---
+++ AtCoder/Beginner_311/c.cpp
@@ -0,0 +1,99 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <atcoder/all>
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+vector<vector<int>> g, gp, scc;
+vector<bool> v;
+deque<int> dq;
+
+void dfs_rev(int x)
+{
+    v[x] = true;
+    for (int i : gp[x])
+        if (!v[i])
+            dfs_rev(i);
+    dq.push_front(x);
+}
+
+void dfs(int x)
+{
+    scc.back().push_back(x);
+    v[x] = false;
+    for (int i : g[x])
+        if (v[i])
+            dfs(i);
+}
+
+void kosaraju(int n)
+{
+    for (int i = 0; i < n; i++)
+        if (!v[i])
+            dfs_rev(i);
+    for (int i : dq)
+        if (v[i])
+        {
+            scc.emplace_back();
+            dfs(i);
+        }
+}
+
+int main()
+{
+    nevikw39;
+    int n;
+    cin >> n;
+    g.assign(n, {});
+    gp.assign(n, {});
+    v.assign(n, false);
+    for (int i = 0, a; i < n; i++)
+    {
+        cin >> a;
+        --a;
+        g[i].push_back(a);
+        gp[a].push_back(i);
+    }
+    kosaraju(n);
+    for (const auto &i : scc)
+        if (i.size() > 1)
+        {
+            cout << i.size() << '\n';
+            for (int j : i)
+                cout << j + 1 << ' ';
+            cout << '\n';
+            return 0;
+        }
+    assert(false);
+}
