a4441fb2fd367fa422ac44a2e6bf3f7662f49ba4
nevikw39 <nevikw39@icloud.com>
2023-07-31 22:43:16 +0800

    Minimum ASCII Delete Sum for Two Strings

---
+++ LeetCode/0712.cpp
@@ -0,0 +1,9 @@
+#include "0712.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.minimumDeleteSum("sea", "eat") << '\n'
+         << sln.minimumDeleteSum("delete", "leet") << '\n';
+    return 0;
+}
