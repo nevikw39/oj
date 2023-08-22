b16d9c715f50b0a5b9cf33dc3a3a67495e09eacf
nevikw39 <nevikw39@icloud.com>
2023-08-22 22:09:58 +0800

    Excel Sheet Column Title

---
+++ LeetCode/0168.cpp
@@ -0,0 +1,10 @@
+#include "0168.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.convertToTitle(1) << '\n'
+         << sln.convertToTitle(28) << '\n'
+         << sln.convertToTitle(701) << '\n';
+    return 0;
+}
