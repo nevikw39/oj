f6b8a6b130ffdd35ae5a33ae6024df3fa0c1b8c4
nevikw39 <nevikw39@icloud.com>
2023-09-21 22:40:34 +0800

    Codeforces Round 898 (Div. 4) A - Short Sort

---
+++ Codeforces/1873/a.c
@@ -0,0 +1,39 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    char abc[4];
+    scanf("%s", abc);
+    puts((*abc != 'a') + (1[abc] != 'b') + (2[abc] != 'c') <= 2 ? "yes" : "no");
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
