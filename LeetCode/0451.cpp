cfa2f203ea626eafa71a856916373e47578cda9b
nevikw39 <nevikw39@cs.nthu.edu.tw>
2024-02-07 10:40:52 +0800

    Sort Characters By Frequency

---
+++ LeetCode/0451.cpp
@@ -0,0 +1,10 @@
+#include "0451.hpp"
+
+int main()
+{
+    Solution sln;
+    cout << sln.frequencySort("tree") << '\n'
+         << sln.frequencySort("cccaaa") << '\n'
+         << sln.frequencySort("Aabb") << '\n';
+    return 0;
+}
