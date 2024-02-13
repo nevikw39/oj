36bd59aabddec9def1e28395b29eacb24f8ddd49
nevikw39 <nevikw39@cs.nthu.edu.tw>
2024-02-13 22:50:17 +0800

    Codeforces Round 925 (Div. 3) A - Recovering a Small String

---
+++ Codeforces/1931/a.cpp
@@ -0,0 +1,73 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+constexpr int N = 78;
+
+string arr[N];
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << arr[n - 3] << '\n';
+}
+
+int main()
+{
+    nevikw39;
+    for (char a = 'a'; a <= 'z'; a++)
+    {
+        string s;
+        s.push_back(a);
+        for (char b = 'a'; b <= 'z'; b++)
+        {
+            s.push_back(b);
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                s.push_back(c);
+                int x = a + b + c - 'a' * 3;
+                if (arr[x].empty() || arr[x] > s)
+                    arr[x] = s;
+                s.pop_back();
+            }
+            s.pop_back();
+        }
+    }
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
