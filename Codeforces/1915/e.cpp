54b8f7e366a7b5d9ac293708720f7bf9345ebee6
nevikw39 <nevikw39@cs.nthu.edu.tw>
2024-02-19 20:34:36 +0800

    Codeforces Round 918 (Div. 4) E - Romantic Glasses

---
+++ Codeforces/1915/e.cpp
@@ -0,0 +1,73 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    bool flag = false;
+    for (int &i : a)
+    {
+        cin >> i;
+        if (flag)
+            i *= -1;
+        flag ^= true;
+    }
+    int64_t sum = 0;
+    set<int64_t> s;
+    s.insert(0);
+    for (int i : a)
+    {
+        sum += i;
+        if (s.count(sum))
+        {
+            cout << "yes\n";
+            return;
+        }
+        s.insert(sum);
+    }
+    cout << "no\n";
+}
+
+int main()
+{
+    nevikw39;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
