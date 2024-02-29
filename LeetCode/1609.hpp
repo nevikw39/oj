09d97da064669dc240835660598b4b74fc5a1251
nevikw39 <nevikw39@cs.nthu.edu.tw>
2024-02-29 11:25:40 +0800

    Even Odd Tree

---
+++ LeetCode/1609.hpp
@@ -0,0 +1,61 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#ifndef nevikw39
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#define __builtin_sprintf sprintf
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#include <bits/extc++.h>
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type, typename F = typename detail::default_hash_fn<K>::type>
+using _hash = gp_hash_table<K, M, F>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+class Solution
+{
+    vector<int> v;
+    bool dfs(TreeNode *root, int dep = 0)
+    {
+        if (!root)
+            return true;
+        if (~(root->val - dep) & 1)
+            return false;
+        if (dep == v.size())
+            v.emplace_back(root->val);
+        else
+        {
+            if ((dep & 1) && root->val >= v[dep] || (~dep & 1) && root->val <= v[dep])
+                return false;
+            v[dep] = root->val;
+        }
+        return dfs(root->left, dep + 1) && dfs(root->right, dep + 1);
+    }
+public:
+    bool isEvenOddTree(TreeNode *root)
+    {
+        v.clear();
+        return dfs(root);
+    }
+};
