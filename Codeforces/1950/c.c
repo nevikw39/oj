032c5491deafdf17a9ec967e8476b957037b5cec
nevikw39 <nevikw39@cs.nthu.edu.tw>
2024-03-28 23:19:13 +0800

    Codeforces Round 937 (Div. 4) C - Clock Conversion

---
+++ Codeforces/1950/c.c
@@ -0,0 +1,42 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    int h, m;
+    scanf("%d:%d", &h, &m);
+    printf("%02d:%02d ", h % 12 ?: 12, m);
+    puts(h < 12 ? "AM" : "PM");
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
