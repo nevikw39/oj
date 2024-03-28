e0d190881a1dcc76749e342e30d8757f67e4ac7d
nevikw39 <nevikw39@cs.nthu.edu.tw>
2024-03-28 22:55:15 +0800

    Codeforces Round 937 (Div. 4) A - Stair, Peak, or Neither?

---
+++ Codeforces/1950/a.c
@@ -0,0 +1,46 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    if (a < b && b < c)
+        puts("STAIR");
+    else if (a < b && b > c)
+        puts("PEAK");
+    else
+        puts("NONE");
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
