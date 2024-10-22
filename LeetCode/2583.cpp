b8b991715fb2f708f7136a504436cd36fda005c9
nevikw39 <nevikw39@cs.nthu.edu.tw>
2024-10-22 09:16:45 +0000

    Kth Largest Sum in a Binary Tree

---
+++ LeetCode/2583.cpp
@@ -0,0 +1,17 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#include "2583.hpp"
+
+int main()
+{
+    Solution sln;
+    return 0;
+}
