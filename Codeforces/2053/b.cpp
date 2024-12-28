3b57a27ca7d4345bd13db77620494eb691749066
nevikw39 <nevikw39@cs.nthu.edu.tw>
2024-12-28 15:32:16 +0000

    Good Bye 2024 B - Outstanding Impressionist

---
+++ Codeforces/2053/b.cpp
@@ -0,0 +1,71 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n << 1 | 1), b(n << 1 | 1);
+    vector<pair<int, int>> lr(n);
+    for (auto &[l, r] : lr)
+    {
+        cin >> l >> r;
+        if (l == r)
+        {
+            ++a[l];
+            b[r] = 1;
+        }
+    }
+    partial_sum(ALL(a), a.begin());
+    partial_sum(ALL(b), b.begin());
+    string s;
+    s.reserve(n);
+    for (const auto &[l, r] : lr)
+        if (l == r && a[r] - a[l - 1] > r - l + 1 || l != r && b[r] - b[l - 1] >= r - l + 1)
+            s.push_back('0');
+        else
+            s.push_back('1');
+    cout << s << '\n';
+}
+
+int main()
+{
+    nevikw39;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
