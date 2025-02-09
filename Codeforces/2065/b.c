f0e884265bb375fde1dc4e7789f85d40642b0cf1
nevikw39 <nevikw39@cs.nthu.edu.tw>
2025-02-09 23:10:56 +0800

    Codeforces Round 1003 (Div. 4) B - Skibidus and Ohio

---
+++ Codeforces/2065/b.c
@@ -0,0 +1,50 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    char s[101], *ptr = s + 1;
+    scanf("%100s", s);
+    while (*ptr)
+    {
+        if (*(ptr - 1) == *ptr)
+        {
+            puts("1");
+            return;
+        }
+        ++ptr;
+    }
+    printf("%ld\n", ptr - s);
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
