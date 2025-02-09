076c8e43acaf6ba0c617261fc0f7b48978487430
nevikw39 <nevikw39@cs.nthu.edu.tw>
2025-02-09 22:53:32 +0800

    Codeforces Round 1003 (Div. 4) A - Skibidus and Amog'u

---
+++ Codeforces/2065/a.c
@@ -0,0 +1,44 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    char w[11];
+    scanf("%10s", w);
+    char *ptr = w + strlen(w) - 2;
+    *ptr++ = 'i';
+    *ptr = 0;
+    puts(w);
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
