d5d3c34107333de332dc1b4ca255c6469483e28d
nevikw39 <nevikw39@cs.nthu.edu.tw>
2025-02-18 15:50:27 +0800

    Construct Smallest Number From DI String

---
+++ LeetCode/2375.cpp
@@ -0,0 +1,9 @@
+#include "2375.hpp"
+
+int main()
+{
+    Solution sln;
+    assert(sln.smallestNumber("IIIDIDDD") == "123549876");
+    assert(sln.smallestNumber("DDD") == "4321");
+    return 0;
+}
