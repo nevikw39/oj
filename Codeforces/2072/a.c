9bad1c5418756090e7060e0702458f4897e702b5
nevikw39 <nevikw39@cs.nthu.edu.tw>
2025-02-28 23:05:20 +0800

    Codeforces Round 1006 (Div. 3) A - New World, New Me, New Array

---
+++ Codeforces/2072/a.c
@@ -0,0 +1,43 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    int n, k, p;
+    scanf("%d%d%d", &n, &k, &p);
+    if (k < 0)
+        k *= -1;
+    printf("%d\n", k <= p * n ? (k + p - 1) / p : -1);
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
