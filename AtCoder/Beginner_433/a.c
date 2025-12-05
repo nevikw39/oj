daa54d4872f8200e431c6438586de49a28f90505
nevikw39 <nevik.w39@gmail.com>
2025-12-05 16:16:12 +0800

    AtCoder Beginner Contest 433 A - Happy Birthday! 4

---
+++ AtCoder/Beginner_433/a.c
@@ -0,0 +1,34 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int x, y, z;
+    scanf("%d%d%d", &x, &y, &z);
+    if (!((x - y * z) % (z - 1)) && x >= y * z)
+        puts("Yes");
+    else
+        puts("No");
+    return 0;
+}
