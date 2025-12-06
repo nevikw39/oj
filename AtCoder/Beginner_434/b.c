dba13955fe2901aaacfd8c1f9735777e84002ca2
nevikw39 <nevik.w39@gmail.com>
2025-12-06 16:39:34 +0800

    AtCoder Beginner Contest 434 B - Bird Watching

---
+++ AtCoder/Beginner_434/b.c
@@ -0,0 +1,40 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    int cnt[m] = {}, sz[m] = {};
+    while (n--)
+    {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        ++cnt[a - 1];
+        sz[a - 1] += b;
+    }
+    for (int i = 0; i < m; i++)
+        printf("%.6lf\n", 1. * i[sz] / i[cnt]);
+    return 0;
+}
