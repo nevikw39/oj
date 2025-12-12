ac4e58115f8df40b145224079d851187baa17572
nevikw39 <nevik.w39@gmail.com>
2025-12-12 15:34:44 +0800

    Codeforces Round 1065 (Div. 3) A - Shizuku Hoshikawa and Farm Legs

---
+++ Codeforces/2171/a.c
@@ -0,0 +1,41 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n & 1 ? 0 : n / 4 + 1);
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
