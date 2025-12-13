ba863d49f55643d202fbb1ef78e72bfda3b8f26b
nevikw39 <nevik.w39@gmail.com>
2025-12-13 20:06:58 +0800

    AtCoder Beginner Contest 436 A - o-padding

---
+++ AtCoder/Beginner_436/a.c
@@ -0,0 +1,35 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int n;
+    char s[101];
+    scanf("%d%s", &n, s);
+    n -= strlen(s);
+    while (n--)
+        putchar('o');
+    puts(s);
+    return 0;
+}
