c449080336772376d8c8f0e1e90acd21fe19fcc6
nevikw39 <nevik.w39@gmail.com>
2025-12-13 20:14:32 +0800

    AtCoder Beginner Contest 436 B - Magic Square

---
+++ AtCoder/Beginner_436/b.c
@@ -0,0 +1,49 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n][n];
+    memset(a, 0, sizeof(int) * n * n);
+    for (int i = 1, x = 0, y = n - 1 >> 1; i <= n * n; i++)
+    {
+        a[x][y] = i;
+        if (a[(x - 1 + n) % n][(y + 1) % n])
+            x = (x + 1) % n;
+        else
+        {
+            x = (x - 1 + n) % n;
+            y = (y + 1) % n;
+        } 
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            printf("%d ", a[i][j]);
+        putchar('\n');
+    }
+    return 0;
+}
