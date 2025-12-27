60df603c569454ddac462f4c134455717d124ee4
nevikw39 <nevik.w39@gmail.com>
2025-12-27 22:56:43 +0800

    Good Bye 2025 A - Yes or Yes

---
+++ Codeforces/2178/a.c
@@ -0,0 +1,45 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    char s[101];
+    scanf("%s", s);
+    int y = 0;
+    for (char *ptr = s; *ptr;)
+        if (*ptr++ == 'Y')
+            ++y;
+    puts(y < 2 ? "yes" : "no");
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
