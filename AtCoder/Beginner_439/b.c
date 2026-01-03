148276cf6d0897bd3dca8c5e4913567e326409f9
nevikw39 <nevik.w39@gmail.com>
2026-01-03 20:08:42 +0800

    AtCoder Beginner Contest 439 B - Happy Number

---
+++ AtCoder/Beginner_439/b.c
@@ -0,0 +1,38 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    while (n > 9)
+    {
+        int sum = 0;
+        for (int i = n; i; i /= 10)
+            sum += (i % 10) * (i % 10);
+        n = sum;
+    }
+    puts(n == 1 ? "Yes" : "No");
+    return 0;
+}
