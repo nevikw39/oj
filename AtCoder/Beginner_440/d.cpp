f9fff35eb3865685df14d871a1bdfac4d84bd474
nevikw39 <nevik.w39@gmail.com>
2026-01-10 21:16:42 +0800

    AtCoder Beginner Contest 440 D - Forbidden List 2

---
+++ AtCoder/Beginner_440/d.cpp
@@ -0,0 +1,58 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <atcoder/all>
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+int main()
+{
+    nevikw39;
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    std::copy_n(istream_iterator<int>(cin), n, a.begin());
+    ranges::sort(a);
+    const auto f = [&](int x){ return x - (ranges::upper_bound(a, x) - a.begin()); };
+    while (q--)
+    {
+        int x, y, m = 0;
+        cin >> x >> y;
+        const int pre = f(x - 1);
+        for (int jump = log2(x + y + n); ~jump; --jump)
+            if (f(m | (1 << jump)) < pre + y)
+                m |= 1 << jump;
+        cout << m + 1 << '\n';
+    }
+    return 0;
+}
