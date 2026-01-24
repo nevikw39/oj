86fac2174a5dc83c6fa5cc2e72cf8db414c00ed4
nevikw39 <nevik.w39@gmail.com>
2026-01-24 20:04:03 +0800

    AtCoder Beginner Contest 442 A - Count

---
+++ AtCoder/Beginner_442/a.c
@@ -0,0 +1,36 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    char s[11];
+    scanf("%10s", s);
+    int dots = 0;
+    for (char *c = s; *c; c++)
+        if (*c == 'i' || *c == 'j')
+            ++dots;
+    printf("%d\n", dots);
+    return 0;
+}
