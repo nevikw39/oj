de7d50877a9cbc1ededdd91e95aa2b4e58c8b46e
nevikw39 <nevik.w39@gmail.com>
2026-02-08 21:44:24 +0800

    AtCoder Regular Contest 214 B - Missing Number in Graph

---
+++ AtCoder/Reguler_214/b.cpp
@@ -0,0 +1,86 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <atcoder/all>
+#include <bits/extc++.h>
+#ifndef nevikw39
+#define nevikw39 cin.tie(nullptr)->sync_with_stdio(false)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct
+{
+    template <typename T>
+    auto &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T, typename Cmp = greater<T>, typename Tag = pairing_heap_tag>
+using _heap = __gnu_pbds::priority_queue<T, Cmp, Tag>;
+template <typename K, typename M = null_type>
+using _hash = gp_hash_table<K, M>;
+template <typename K, typename M = null_type, typename Cmp = less<K>, typename T = rb_tree_tag>
+using _tree = tree<K, M, Cmp, T, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pair<int, int>>> g(n);
+    while (m--)
+    {
+        int a, b, x;
+        cin >> a >> b >> x;
+        --a;
+        --b;
+        g[a].emplace_back(b, x);
+        g[b].emplace_back(a, x);
+    }
+    if (n & 1)
+    {
+        println("-1");
+        return;
+    }
+    queue<int> q;
+    q.emplace(0);
+    vector<int> spanning_tree_xor(n, -1);
+    spanning_tree_xor[0] = 0;
+    while (q.size())
+    {
+        const int f = q.front();
+        q.pop();
+        for (const auto &[i, x] : g[f])
+            if (!~spanning_tree_xor[i])
+            {
+                spanning_tree_xor[i] = spanning_tree_xor[f] ^ x;
+                q.emplace(i);
+            }
+    }
+    int s = 0;
+    for (int i : views::iota(0, n))
+        s ^= (i + 1) ^ spanning_tree_xor[i];
+    println("{}", s);
+}
+
+int main()
+{
+    nevikw39;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
