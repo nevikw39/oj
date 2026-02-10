974c62b1e2b9c05ea9aa63f57ebfa14ebdf54fb0
nevikw39 <nevik.w39@gmail.com>
2026-02-10 19:21:01 +0800

    AtCoder Weekday Contest 0002 Beta C - Observing Plant Growth

---
+++ AtCoder/Weekday_0002/c.c
@@ -0,0 +1,40 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+inline int bwmax(int l, int r) { return l ^ ((l ^ r) & -(l < r)); }
+
+int main()
+{
+    int n, m, d = 0;
+    scanf("%d%d", &n, &m);
+    while (n--)
+    {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        d = bwmax(d, (m - a + b - 1) / b);
+    }
+    printf("%d\n", d);
+    return 0;
+}
