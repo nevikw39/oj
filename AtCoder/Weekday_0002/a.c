7379ddcc6712a5a61105896caeb746bcb239f2b5
nevikw39 <nevik.w39@gmail.com>
2026-02-10 19:03:38 +0800

    AtCoder Weekday Contest 0002 Beta A - Organizing the Bookshelf

---
+++ AtCoder/Weekday_0002/a.c
@@ -0,0 +1,41 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int n, k, x = -1;
+    scanf("%d%d", &n, &k);
+    for (int i = 0, a; i < n; i++)
+    {
+        scanf("%d", &a);
+        if (a == k)
+        {
+            x = i + 1;
+            break;
+        }
+    }
+    printf("%d\n", x);
+    return 0;
+}
