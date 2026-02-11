18cb567662ca6dfacaf18e35e9e86e293dab5011
nevikw39 <nevik.w39@gmail.com>
2026-02-11 19:52:47 +0800

    AtCoder Weekday Contest 0003 Beta B - Line of Handshakes

---
+++ AtCoder/Weekday_0003/b.c
@@ -0,0 +1,39 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int n, cnt = 0;
+    scanf("%d\n", &n);
+    char l[2] = {}, r[2] = {};
+    while (n--)
+    {
+        scanf("%c %c\n", &l[n & 1], &r[n & 1]);
+        if (l[n & 1] == r[~n & 1])
+            ++cnt;
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
