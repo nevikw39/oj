c5cd9c652b378227951afb1d5b077032920841ca
nevikw39 <nevik.w39@gmail.com>
2026-02-11 19:59:38 +0800

    AtCoder Weekday Contest 0003 Beta A - Product Quality Evaluation

---
+++ AtCoder/Weekday_0003/a.c
@@ -0,0 +1,41 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    int n, cnt = 0;
+    int64_t k;
+    scanf("%d%" PRId64 , &n, &k);
+    while (n--)
+    {
+        int a;
+        int64_t b;
+        scanf("%d%" PRId64, &a, &b);
+        if (a * b >= k)
+            ++cnt;
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
