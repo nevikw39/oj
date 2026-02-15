219ff0a325cba86a4c95f2dc9258c0e6c71924a7
nevikw39 <nevik.w39@gmail.com>
2026-02-15 23:06:37 +0800

    Codeforces Round 1080 (Div. 3) A - Sieve of Erato67henes

---
+++ Codeforces/2195/a.c
@@ -0,0 +1,49 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+// note: 67 is a prime
+void solve()
+{
+    int n;
+    scanf("%d", &n);
+    bool flag = false;
+    for (int a; n--;)
+    {
+        scanf("%d", &a);
+        if (a == 67)
+            flag = true;
+    }
+    puts(flag ? "yes" : "no");
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
