0067062e4f7474208acca6dd33096c4e7dc7f927
nevikw39 <nevik.w39@gmail.com>
2026-03-14 20:08:18 +0800

    AtCoder Beginner Contest 449 A - π

---
+++ AtCoder/Beginner_449/a.c
@@ -0,0 +1,33 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+int main()
+{
+    const long double PI4 = atanl(1.0L);
+    int d;
+    scanf("%d", &d);
+    printf("%.8Lf\n", PI4 * d * d);
+    return 0;
+}
