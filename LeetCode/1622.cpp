9c70febaf3cd1b8662e66fd101f2b36fb42d60e3
nevikw39 <nevik.w39@gmail.com>
2026-03-15 13:20:30 +0800

    Fancy Sequence

---
+++ LeetCode/1622.cpp
@@ -0,0 +1,18 @@
+#include "1622.hpp"
+
+int main()
+{
+    Fancy fancy{};
+    fancy.append(2);   // fancy sequence: [2]
+    fancy.addAll(3);   // fancy sequence: [2+3] -> [5]
+    fancy.append(7);   // fancy sequence: [5, 7]
+    fancy.multAll(2);  // fancy sequence: [5*2, 7*2] -> [10, 14]
+    fancy.getIndex(0); // return 10
+    fancy.addAll(3);   // fancy sequence: [10+3, 14+3] -> [13, 17]
+    fancy.append(10);  // fancy sequence: [13, 17, 10]
+    fancy.multAll(2);  // fancy sequence: [13*2, 17*2, 10*2] -> [26, 34, 20]
+    fancy.getIndex(0); // return 26
+    fancy.getIndex(1); // return 34
+    fancy.getIndex(2); // return 20
+    return 0;
+}
