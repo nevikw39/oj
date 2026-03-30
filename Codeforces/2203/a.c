9c12c2d51e4366cfbcc8df637cbbc80ef03cedce
nevikw39 <nevik.w39@gmail.com>
2026-03-30 19:10:36 +0800

    Educational Codeforces Round 187 A - Towers of Boxes

---
+++ Codeforces/2203/a.c
@@ -0,0 +1,42 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    int n, m, d;
+    scanf("%d%d%d", &n, &m, &d);
+    const int x = d / m + 1;
+    printf("%d\n", (n + x - 1) / x);
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
