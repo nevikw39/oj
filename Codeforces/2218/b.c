72563ecc893747a015d0f699d7be39ebfcb539e1
nevikw39 <nevik.w39@gmail.com>
2026-04-04 23:00:07 +0800

    Codeforces Round 1090 (Div. 4) B - The 67th 6-7 Integer Problem

---
+++ Codeforces/2218/b.c
@@ -0,0 +1,47 @@
+/**                  _ _              _____ ___
+ *  _ __   _____   _(_) | ____      _|___ // _ \
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/
+ **/
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifndef nevikw39
+#else
+#pragma message("hello, nevikw39")
+#endif
+#pragma message("GL; HF!")
+
+void solve()
+{
+    int sum = 0, mx = -67;
+    for (int i = 0, a; i < 7; i++)
+    {
+        scanf("%d", &a);
+        sum -= a;
+        if (mx < a)
+            mx = a;
+    }
+    printf("%d\n", sum + (mx << 1));
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+        solve();
+    return 0;
+}
